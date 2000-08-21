07fbaa6d890b2f7f3cd70802c3c3907c61fc86d5
aiv <aiv@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2000-08-21 20:43:44 +0000

    Added Preview (like map editor)

--- source/trunk/prog/model/form_Model.pas
+++ source/trunk/prog/model/form_Model.pas
@@ -39,8 +39,9 @@ interface
 
 uses
   Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
-  QkObjects, QkFileObjects, TB97, QkForm, QkImages, Python, Game,
-  StdCtrls, EnterEditCtrl, ExtCtrls;
+  QkObjects, QkFileObjects, TB97, QkForm, QkImages, Python, PyMapView,
+  StdCtrls, EnterEditCtrl, ExtCtrls, PyMath, qmatrices, qmath, QkMdlObject,
+  QkTextures, QkSin, CursorScrollBox;
 
 type
   TFQMdl = class(TQForm1)
@@ -49,19 +50,25 @@ type
     Label2: TLabel;
     Button1: TButton;
     EnterEdit1: TEnterEdit;
+    Panel2: TPanel;
     procedure Button1Click(Sender: TObject);
     procedure EnterEdit1Accept(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
   private
-    { Private Stuff }
+    FRoot: QMdlObject;
+    procedure ScrollBox1Paint(Sender: TObject; DC: Integer; const rcPaint: TRect);
   protected
     function AssignObject(Q: QFileObject; State: TFileObjectWndState) : Boolean; override;
+    procedure ReadSetupInformation(Level: Integer); override;
   public
+    ScrollBox1: TPyMapView;
     procedure wmInternalMessage(var Msg: TMessage); message wm_InternalMessage;
   end;
 
 implementation
 
-uses QuarkX, Setup, PyForms, Undo, QkModel;
+uses QuarkX, Setup, PyForms, Undo, QkModel, QkMapObjects, Qk3D;
 
 {$R *.dfm}
 
@@ -70,9 +77,19 @@ begin
   Result:=(Q is QModel) and (State<>cmWindow) and inherited AssignObject(Q, State);
 end;
 
+procedure TFQMdl.ReadSetupInformation(Level: Integer);
+begin
+  inherited;
+  ScrollBox1.Invalidate;
+  ScrollBox1.Color:=MapColors(lcVueXZ);
+end;
+
 procedure TFQMdl.wmInternalMessage(var Msg: TMessage);
 var
- S: String;
+  S: String;
+  Min, Max, D: TVect;
+  Racine: QObject;
+  M: TMatrixTransformation;
 begin
   if Msg.wParam=wp_AfficherObjet then begin
     if FileObject=Nil then
@@ -89,6 +106,44 @@ begin
     if FileObject<>Nil then
       S:=FileObject.Specifics.Values['FileName'];
     EnterEdit1.Text:=S;
+    if FileObject=Nil then
+      Exit;
+    S:=FileObject.Specifics.Values['Root'];
+    if S='' then
+      Exit;  { no data }
+    Racine:=FileObject.SubElements.FindName(S);
+    if (Racine=Nil) or not (Racine is QMdlObject) then
+      Exit;  { no data }
+    Racine.ClearAllSelection;
+    Min.X:=-10;
+    Min.Y:=-10;
+    Min.Z:=-10;
+    Max.X:=+10;
+    Max.Y:=+10;
+    Max.Z:=+10;
+    QMdlObject(Racine).ChercheExtremites(Min, Max);
+
+    D.X:=(ScrollBox1.ClientWidth-20)/(Max.X-Min.X);
+    D.Y:=(ScrollBox1.ClientHeight-18)/(Max.Y-Min.Y);
+    if D.Y<D.X then
+      D.X:=D.Y;
+    ScrollBox1.MapViewProj.Free;
+    ScrollBox1.MapViewProj:=Nil;
+    M:=MatriceIdentite;
+    // Change To YZ View ie rotate around x axis by 90 deg.
+    M[1,1]:=D.X;
+    M[2,2]:=cos(90*(pi / 180))*D.X;
+    M[3,2]:=sin(90*(pi / 180))*D.X;
+    M[2,3]:=-M[3,2];
+    M[3,3]:=M[2,2];
+    ScrollBox1.MapViewProj:=GetMatrixCoordinates(M);
+    ScrollBox1.HorzScrollBar.Range:=ScrollBox1.ClientWidth;
+    ScrollBox1.VertScrollBar.Range:=ScrollBox1.ClientHeight;
+    D.X:=(Min.X+Max.X)*0.5;
+    D.Y:=(Min.Y+Max.Y)*0.5;
+    D.Z:=(Min.Z+Max.Z)*0.5;
+    FRoot:=QMdlObject(Racine);
+    ScrollBox1.CentreEcran:=D;
   end else
     inherited;
 end;
@@ -108,6 +163,50 @@ begin
   S:=EnterEdit1.Text;
   Undo.Action(Q, TSpecificUndo.Create(LoadStr1(615), 'FileName',
     S, sp_AutoSuppr, Q));
+end;
+
+procedure TFQMdl.FormCreate(Sender: TObject);
+begin
+  inherited;
+  ScrollBox1:=TPyMapView.Create(Self);
+  ScrollBox1.MapViewObject^.Parent:=Nil;
+  ScrollBox1.Parent:=Panel2;
+  ScrollBox1.Align:=alClient;
+ {FOldPaint:=ScrollBox1.OnPaint;}
+  ScrollBox1.OnPaint:=ScrollBox1Paint;
+end;
+
+procedure TFQMdl.ScrollBox1Paint(Sender: TObject; DC: Integer; const rcPaint: TRect);
+var
+  Pen: HPen;
+  Brush: HBrush;
+begin
+  if FRoot=Nil then
+    Exit;
+ {FOldPaint(Sender, PaintInfo);}
+  Canvas.Handle:=DC;
+  try
+    SetupWhiteOnBlack(Info.DefWhiteOnBlack);
+    ScrollBox1.MapViewProj.SetAsCCoord(DC);
+    Pen:=SelectObject(Info.DC, GetStockObject(Null_Pen));
+    Brush:=SelectObject(Info.DC, GetStockObject(Null_Brush));
+    Info.GreyBrush:=CreatePen(ps_Solid, 0, MapColors(lcOutOfView));
+    try
+      FRoot.Dessiner;
+    finally
+      SelectObject(Info.DC, Brush);
+      SelectObject(Info.DC, Pen);
+      DeleteObject(Info.GreyBrush);
+    end;
+  finally
+    Canvas.Handle:=0;
+  end;
+end;
+
+procedure TFQMdl.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  FRoot:=Nil;
+  inherited;
 end;
 
 end.
