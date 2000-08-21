7d0c281128ed72443d4f468598a086145aaff628
aiv <aiv@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2000-08-21 21:17:58 +0000

    Changed to toolbar instead of menuitems

--- runtime/trunk/plugins/mdllocking.py
+++ runtime/trunk/plugins/mdllocking.py
@@ -23,39 +23,50 @@ from quarkpy.mdlmgr import *
 
 def lockxclick(m):
   editor = mapeditor()
+  quarkpy.qtoolbar.toggle(m)
   if editor.lock_x == 0:
-    m.state = 2
     editor.lock_x = 1
   else:
-    m.state = 0
     editor.lock_x = 0
 
 def lockyclick(m):
   editor = mapeditor()
+  quarkpy.qtoolbar.toggle(m)
   if editor.lock_y == 0:
-    m.state = 2
     editor.lock_y = 1
   else:
-    m.state = 0
     editor.lock_y = 0
 
 def lockzclick(m):
   editor = mapeditor()
+  quarkpy.qtoolbar.toggle(m)
   if editor.lock_z == 0:
-    m.state = 2
     editor.lock_z = 1
   else:
-    m.state = 0
     editor.lock_z = 0
 
-Lock_X = qmenu.item("Lock &X", lockxclick, "lock x axis movement")
-Lock_Y = qmenu.item("Lock &Y", lockyclick, "lock y axis movement")
-Lock_Z = qmenu.item("Lock &Z", lockzclick, "lock z axis movement")
+#Lock_X = qmenu.item("Lock &X", lockxclick, "lock x axis movement")
+#Lock_Y = qmenu.item("Lock &Y", lockyclick, "lock y axis movement")
+#Lock_Z = qmenu.item("Lock &Z", lockzclick, "lock z axis movement")
 
-quarkpy.mdlcommands.items.append(quarkpy.qmenu.sep)
-quarkpy.mdlcommands.items.append(Lock_X)
-quarkpy.mdlcommands.items.append(Lock_Y)
-quarkpy.mdlcommands.items.append(Lock_Z)
-quarkpy.mdlcommands.shortcuts["Shift+X"] = Lock_X
-quarkpy.mdlcommands.shortcuts["Shift+Y"] = Lock_Y
-quarkpy.mdlcommands.shortcuts["Shift+Z"] = Lock_Z
+class AxisLockBar(ToolBar):
+    "Axis Lock Toolbar"
+
+    Caption = "Axis Lock"
+
+    def buildbuttons(self, layout):
+        LockXBtn = qtoolbar.button(lockxclick, "Lock X Axis", ico_mdled, 0)
+        LockYBtn = qtoolbar.button(lockyclick, "Lock Y Axis", ico_mdled, 1)
+        LockZBtn = qtoolbar.button(lockzclick, "Lock Z Axis", ico_mdled, 2)
+        layout.buttons.update({"lockx": LockXBtn, "locky": LockYBtn,"lockz": LockZBtn})
+        return [LockXBtn, LockYBtn, LockZBtn]
+
+#quarkpy.mdlcommands.items.append(quarkpy.qmenu.sep)
+#quarkpy.mdlcommands.items.append(Lock_X)
+#quarkpy.mdlcommands.items.append(Lock_Y)
+#quarkpy.mdlcommands.items.append(Lock_Z)
+#quarkpy.mdlcommands.shortcuts["Shift+X"] = Lock_X
+#quarkpy.mdlcommands.shortcuts["Shift+Y"] = Lock_Y
+#quarkpy.mdlcommands.shortcuts["Shift+Z"] = Lock_Z
+
+quarkpy.mdltools.toolbars["tb_AxisLock"] = AxisLockBar
