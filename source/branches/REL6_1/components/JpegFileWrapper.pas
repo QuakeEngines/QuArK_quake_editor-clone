7cc86b28482620ae5c913d6f1340a5e3036354d2
alexander <alexander@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2000-09-03 18:12:11 +0000

    committed aiv's debug extensions also to REL6_1

--- source/branches/REL6_1/components/JpegFileWrapper.pas
+++ source/branches/REL6_1/components/JpegFileWrapper.pas
@@ -25,7 +25,12 @@ implementation
 
 Procedure TJpegFileWrapper.Load(input,output: TStream; nocolors:Integer; callback: JPEG_ProgressMonitor);
 begin
+  try
 LoadJPEG(input,output, false, nocolors, callback);
+  except
+    on E: Exception do
+      raise Exception.Createfmt('Exception "%s" in TJpegFileWrapper.Load',[E.message]);
+  end;
 end;
 
 Procedure TJpegFileWrapper.Save(input,output: TStream; quality:Integer; callback: JPEG_ProgressMonitor);
