d9f1bdb69e63992324e592ff53c68ea4ef6ac029
decker_dk <decker_dk@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2000-11-25 20:50:20 +0000

    Misc. updates

--- utils/trunk/fixids.bat
+++ utils/trunk/fixids.bat
@@ -1,1 +1,1 @@
-python.exe fixpasid.py -c -s subs.txt ../source/prog ../source/python ../source/3dfx > output.txt
+python.exe fixpasid.py -c -s subs.txt ../source/prog ../source/prog/model ../source/python ../source/3dfx > output.txt
