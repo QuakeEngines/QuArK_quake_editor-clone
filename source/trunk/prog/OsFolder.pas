45eaab30021a699b1535278784d11ff89dca7f88
tiglari <tiglari@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2001-01-21 06:37:42 +0000

    OsFolder implementation

---
+++ source/trunk/prog/OsFolder.pas
@@ -0,0 +1,62 @@
+(**************************************************************************
+QuArK -- Quake Army Knife -- 3D game editor
+Copyright (C) 1996-99 Armin Rigo
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+Contact the author Armin Rigo by e-mail: arigo@planetquake.com
+or by mail: Armin Rigo, La Cure, 1854 Leysin, Switzerland.
+See also http://www.planetquake.com/quark
+**************************************************************************)
+
+unit OsFolder;
+
+interface
+
+uses
+  ToolBoxGroup, QkObjects, Dialogs, Classes, QkFileObjects;
+
+type
+  QOsFolder = class(QToolBoxGroup)
+    protected
+      procedure ExpandPrep; override;
+    public
+      class function TypeInfo: String; override;
+      class procedure FileObjectClassInfo(var Info: TFileObjectClassInfo); override;
+ end;
+
+
+implementation
+
+class function QOsFolder.TypeInfo;
+begin
+ TypeInfo:='.osfolder';
+end;
+
+class procedure QOsFolder.FileObjectClassInfo(var Info: TFileObjectClassInfo);
+begin
+ inherited;
+ Info.NomClasseEnClair:='Dynamic Toolbox Folder';
+ Include(Info.WndInfo, wiNeverOpen);
+ Info.WndInfo:=[wiSameExplorer];
+end;
+
+
+procedure QOsFolder.ExpandPrep;
+begin
+  ShowMessage('If this was done, something useful would happen');
+end;
+
+end.
