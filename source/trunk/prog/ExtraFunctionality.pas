257556e516236672b55015caef310ca4e47aa3ce
aiv <aiv@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2001-02-02 00:14:42 +0000

    removed IsPathDelimiter cos it don't work with <D5

--- source/trunk/prog/ExtraFunctionality.pas
+++ source/trunk/prog/ExtraFunctionality.pas
@@ -2,22 +2,14 @@ unit ExtraFunctionality;
 
 interface
 
-function IncludeTrailingBackslash(s: string): string;
-function IsPathDelimiter(const S: string; Index: Integer): Boolean;
+function IncludeTrailingBackslash(const S: string): string;
 
 implementation
-
-function IsPathDelimiter(const S: string; Index: Integer): Boolean;
-begin
-  Result := (Index > 0) and (Index <= Length(S)) and (S[Index] = '\')
-    and (ByteType(S, Index) = mbSingleByte);
-end;
 
 function IncludeTrailingBackslash(const S: string): string;
 begin
   Result := S;
-  if not IsPathDelimiter(Result, Length(Result)) then Result :=
-Result + '\';
+  if Result[length(result)]<>'\' then Result := Result + '\';
 end;
 
 end.
