3f1cbf9ecd01cea371ed7534c21c350907bc8afc
tiglari <tiglari@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2001-02-11 08:04:15 +0000

    comments added, some cosmetics

--- runtime/trunk/plugins/mapmicrobrush.py
+++ runtime/trunk/plugins/mapmicrobrush.py
@@ -32,6 +32,19 @@ import quarkpy.dlgclasses
 import mapmadsel
 import quarkx
 
+#
+# A `Live Edit' dialog.  Note the action buttons, which
+#   use a rather convoluted technique to produce their
+#   effects (when a button is bushed,
+#   quarkpy.qmacro.MACRO_zapview is executed with an index
+#   saying which, this calls the appropriate function of the
+#   dialog, which was attached to the editor when executed).
+#
+# A ListBox would be better than a ComboBox for data entry,
+#   but FormCFG.pas doesn't at the moment support ListBoxes.
+#   In principle, this could be fixed.
+#
+
 class MicroKillDlg (quarkpy.dlgclasses.LiveEditDlg):
     #
     # dialog layout
@@ -51,7 +64,7 @@ class MicroKillDlg (quarkpy.dlgclasses.LiveEditDlg):
           Txt = "Micros:"
           Items = "%s"
           Values = "%s"
-          Hint = "These are the brushes that are too thin" $0D "Push buttons on row below for action."
+          Hint = "These are the brushes that are too thin.  Pick one," $0D " then push buttons on row below for action."
         }
 
           
@@ -62,7 +75,7 @@ class MicroKillDlg (quarkpy.dlgclasses.LiveEditDlg):
         Num = "3"
         Macro = "zapview"
         Caps = "IDA"
-        Caption = "Actions:"
+        Txt = "Actions:"
         Hint1 = "Inspect the chosen one"
         Hint2 = "Delete the chosen one"
         Hint3 = "Kill them all"
@@ -85,9 +98,6 @@ class MicroKillDlg (quarkpy.dlgclasses.LiveEditDlg):
     }
     """
 
-
-
-
     def inspect(self):
         index = eval(self.chosen)
         #
@@ -106,6 +116,12 @@ class MicroKillDlg (quarkpy.dlgclasses.LiveEditDlg):
         self.editor.ok(undo,'delete microbrush')
         self.pack.thinnies.remove(thin)   
         self.src["micros"]=''
+        #
+        # This seems to need to be called to get the dialog
+        #   to reset itself with the new data (not quite sure
+        #   why it doesn't happen automatically here, but it
+        #   dosnt seem to)
+        #
         self.datachange(self.dlg)
 
     def zapall(self):
@@ -116,7 +132,11 @@ class MicroKillDlg (quarkpy.dlgclasses.LiveEditDlg):
         self.src["micros"]=''
         self.datachange(self.dlg)
 
-
+#
+# Define the zapview macro here, put the definition into
+#  quarkpy.qmacro, which is where macros called from delphi
+#  live.
+#
 def macro_zapview(self, index=0):
     editor = mapeditor()
     if editor is None: return
@@ -129,7 +149,10 @@ def macro_zapview(self, index=0):
         
 quarkpy.qmacro.MACRO_zapview = macro_zapview
 
-
+#
+# For a face, for each other different face, the vertex furthest
+#  away from the other face must be far enough away/
+#
 def brushIsThin(brush, thick):
     for face in brush.faces:
         for face2 in brush.faces:
@@ -139,7 +162,6 @@ def brushIsThin(brush, thick):
                 sep = 0.0
                 for vert in face.verticesof(brush):
                     sep=max(sep,abs(vert-projectpointtoplane(vert,n,d*n,n)))
-      #              squawk('  '+`sep`)
                 if sep<thick:
                         return 1
     return 0                
@@ -162,18 +184,33 @@ def thinClick(m):
     for brush in editor.Root.findallsubitems("",":p"):
         if brushIsThin(brush,eval(thin)):
             thinnies.append(brush)
-#    if thinnies==[]:
-#        return
     
+    #
+    # Here we start the Live Edit dialog invocation sequence.
+    #  Data to be tracked during the life of the dialog goes
+    #  here.
+    #
     class pack:
         "stick stuff in this"
     pack.thinnies=thinnies
     pack.thin=thin
       
+    #
+    # This loads the relevant data into the dialog, gets
+    #  recalled after changes.
+    #
     def setup(self, pack=pack, editor=editor):
         self.pack=pack
+        #
+        # Part of the convolution for the buttons, to communicate
+        #  which objects methods should be called when one pushed.
+        # Cleaned up in onclosing below.
+        #
         editor.microbrushdlg=self
         
+        #
+        # Names and list-indexes of thin brushes
+        #
         pack.slist = map(lambda obj:obj.shortname, pack.thinnies)
         pack.klist = map(lambda d:`d`, range(len(pack.thinnies)))
 
@@ -183,13 +220,24 @@ def thinClick(m):
 #        self.src["micros"] = pack.klist[0]
         self.src["micros$Items"] = string.join(pack.slist, "\015")
         self.src["micros$Values"] = string.join(pack.klist, "\015")
-
+        #
+        # Note the commas, EF..1 controls take 1-tuples as data
+        #
         self.src["num"]=len(pack.klist),
         self.src["thin"]=eval(pack.thin),
 
+    #
+    # When data is entered, this gets executed.
+    #
     def action(self, pack=pack, editor=editor):
        src = self.src
+       #
+       # note what's been chosen
+       #
        self.chosen = src["micros"]
+       #
+       # see if thinness threshold has been changed
+       #
        newthin, = self.src["thin"]
        if newthin!=pack.thin:
            if newthin==1.0:
@@ -199,12 +247,23 @@ def thinClick(m):
            pack.thinnies=getThin(newthin, editor)
            pack.thin="%.2f"%newthin
            
+    #
+    # Cleanup when dialog closes (not needed if no mess has
+    #  been created)
+    #
     def onclosing(self,editor=editor):
         del editor.microbrushdlg
         
+    #
+    # And here's the invocation. 2nd arg is a label for storing
+    #  position info in setup.qrk.
+    #
     MicroKillDlg(quarkx.clickform, 'microkill', editor, setup, action, onclosing)
 
 
 quarkpy.mapsearch.items.append(qmenu.item('Find &Microbrushes', thinClick))
 
 #$Log$
+#Revision 1.1  2001/02/11 06:44:17  tiglari
+#Version 1
+#
