3de05433a0c3f7d754b474ee0ec66e194ab1d8ba
decker_dk <decker_dk@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2001-02-23 19:25:04 +0000

    Fixed problem with 'idata' in TDisplay.GetInfo being used wrongly.

--- source/trunk/prog/SystemDetails.pas
+++ source/trunk/prog/SystemDetails.pas
@@ -2,6 +2,9 @@
 $Header$
 ----------- REVISION HISTORY ------------
 $Log$
+Revision 1.3  2001/02/14 20:45:10  aiv
+Added Logging of Python version.
+
 Revision 1.2  2001/02/11 22:26:44  aiv
 Added CVS Headers
 
@@ -1146,7 +1149,8 @@ end;
 procedure TDisplay.GetInfo;
 var
   rk :string;
-  idata,bdata :pchar;
+  bdata :pchar;
+  idata:integer;
   sl :tstringlist;
   i :integer;
   DevMode : TDevMode;
@@ -1367,10 +1371,8 @@ begin
               end;
             if ValueExists(rvHardware+'.'+rvHWMem) then
               try
-                idata:=stralloc(255);
                 readbinarydata(rvHardware+'.'+rvHWMem,idata,4);
-                FMemory.Add(inttostr(integer(idata)));
-                strdispose(idata);
+                FMemory.Add(inttostr(idata));
               except
               end;
             closekey;
