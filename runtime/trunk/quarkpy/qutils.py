34dde58ebb8db10c0974233949b99c3723e19958
tiglari <tiglari@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2001-03-20 11:01:15 +0000

    hotkey fix (now in General)

--- runtime/trunk/quarkpy/qutils.py
+++ runtime/trunk/quarkpy/qutils.py
@@ -429,12 +429,12 @@ def debug(text):
     sys.stderr.write(text+"\n")
 
 def MapHotKey(keytag, keyfunc, menu):
-    key = quarkx.setupsubset(SS_MAP,"HotKeys")[keytag]
+    key = quarkx.setupsubset(SS_GENERAL,"HotKeys")[keytag]
     if key:
         menu.shortcuts[key] = keyfunc
 
 def MapHotKeyList(keytag, keyfunc, list):
-    key = quarkx.setupsubset(SS_MAP,"HotKeys")[keytag]
+    key = quarkx.setupsubset(SS_GENERAL,"HotKeys")[keytag]
     if key:
         list[key] = keyfunc
 
@@ -448,6 +448,9 @@ plugins.LoadPlugins("Q_")
 
 # ----------- REVISION HISTORY ------------
 #$Log$
+#Revision 1.9  2001/03/20 07:59:40  tiglari
+#customizable hot key support
+#
 #Revision 1.8  2001/03/01 19:15:14  decker_dk
 #changed loadmapeditor() so it reads the 'BezierPatchSupport' and 'BezierPatchPluginPrefixes' instead.
 #
