378707566f4d2447c76df132fe05e702abdd3c5a
tiglari <tiglari@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2001-06-14 12:16:47 +0000

    nameing dialog, multiple view support (pick last clicked on)

--- runtime/trunk/plugins/mapcamerapos.py
+++ runtime/trunk/plugins/mapcamerapos.py
@@ -32,6 +32,21 @@ from quarkpy import qhandles
 from quarkpy.maphandles import *
         
 
+def get3DView(editor):
+    views = filter(lambda v:v.info["type"]=="3D",editor.layout.views)
+    if len(views)==0:
+        quarkx.msgbox("Open a 3D view",2,4)
+        return
+    elif len(views)==1:
+        return views[0]
+    for view in views:
+        try:
+            if view is editor.last3DView:
+                return view
+        except (AttributeError):
+            pass
+    return views[0]
+
 #
 # The menu redefinition trick, as discussed in the plugin tutorial
 #  in the infobase.  'o' is the duplicator object
@@ -50,23 +65,15 @@ def camposmenu(o, editor, oldmenu=mapentities.DuplicatorType.menu.im_func):
     #  finds, is there a better thing to do?
     #
     def setViewClick(m,o=o,editor=editor):
-        for v in editor.layout.views:
-           if v.info["type"] == "3D":
-               view=v
-               break
-        else:
-            quarkx.msgbox("Open a 3d view",2,4)
+        view = get3DView(editor)
+        if view is None:
             return
         view.cameraposition = o.origin, o["yaw"][0], o["pitch"][0]          
         editor.invalidateviews()
 
     def storeViewClick(m,o=o,editor=editor):
-        for v in editor.layout.views:
-           if v.info["type"] == "3D":
-               view=v
-               break
-        else:
-            quarkx.msgbox("Open a 3d view",2,4)
+        view = get3DView(editor)
+        if view is None:
             return
         pos, yaw, pitch = view.cameraposition
         undo = quarkx.action()
@@ -114,6 +121,42 @@ mapduplicator.DupCodes.update({
   "cameraposition":  CamPosDuplicator,
 })
 
+
+class NameDialog(SimpleCancelDlgBox):
+    "A simple dialog box to enter a name."
+
+    endcolor = AQUA
+    size = (330,120)
+    dfsep = 0.45
+    dlgdef = """
+      {
+        Style = "9"
+        Caption = "Enter name"
+        sep: = {Typ="S" Txt=" "}    // some space
+        name: = {
+          Txt=" Enter the name :"
+          Typ="E"
+        }
+        sep: = {Typ="S" Txt=" "}    // some space
+        sep: = {Typ="S"}    // a separator line
+        cancel:py = { }
+      }
+    """
+
+    def __init__(self, form, action, initialvalue=None):
+        src = quarkx.newobj(":")
+        if initialvalue is not None:
+           src["name"]=initialvalue
+        self.action=action
+        SimpleCancelDlgBox.__init__(self, form, src)
+
+    def ok(self):
+        name = self.src["name"]
+        if name is None:
+            name="Camera Position"
+        self.action(name)
+
+
 #
 # And more menu redefinition, this time for the
 #  EyePositionMap handle defined in maphandles.py.
@@ -121,29 +164,35 @@ mapduplicator.DupCodes.update({
 def newEyePosMenu(self, editor, view):
     
     def addClick(m,self=self,editor=editor):
-        view = self.view3D
-        #
-        # NB: elsewhere in the code, 'yaw' tends to
-        # be misnamed as 'roll'
-        #
-        pos, yaw, pitch = self.view3D.cameraposition
-        camdup = quarkx.newobj("Camera Position:d")
-        camdup["macro"] = "cameraposition"
-        pozzies = editor.Root.findname("Camera Positions:g")
-        undo=quarkx.action()
-        if pozzies is None:
-            pozzies = quarkx.newobj("Camera Positions:g")
-            undo.put(editor.Root,pozzies)
-        undo.put(pozzies, camdup)
-        undo.setspec(camdup,"origin",str(pos))
-        undo.setspec(camdup,"yaw",(yaw,))
-        undo.setspec(camdup,"pitch",(pitch,))
-        editor.ok(undo,'add camera position')
-    
+        def action(name,self=self,editor=editor):
+            view = self.view3D
+            #
+            # NB: elsewhere in the code, 'yaw' tends to
+            # be misnamed as 'roll'
+            #
+            pos, yaw, pitch = self.view3D.cameraposition
+            camdup = quarkx.newobj(name+":d")
+            camdup["macro"] = "cameraposition"
+            pozzies = editor.Root.findname("Camera Positions:g")
+            undo=quarkx.action()
+            if pozzies is None:
+                pozzies = quarkx.newobj("Camera Positions:g")
+                undo.put(editor.Root,pozzies)
+            undo.put(pozzies, camdup)
+            undo.setspec(camdup,"origin",str(pos))
+            undo.setspec(camdup,"yaw",(yaw,))
+            undo.setspec(camdup,"pitch",(pitch,))
+            editor.ok(undo,'add camera position')
+         
+        NameDialog(quarkx.clickform,action,"Camera Position")
+         
     item = qmenu.item('Add position',addClick)
     return [item]
 
 EyePositionMap.menu = newEyePosMenu
 
 # $Log$
+# Revision 1.1  2001/06/13 22:28:09  tiglari
+# kickoff
+#
 #
