b535679922b35919f99806326b45c601a39cffba
decker_dk <decker_dk@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2001-06-24 14:47:58 +0000

    Can now also replace specific-names.

--- runtime/trunk/plugins/mapargreplacer.py
+++ runtime/trunk/plugins/mapargreplacer.py
@@ -1,10 +1,8 @@
-"""   QuArK  -  Quake Army Knife
-
+"""
+QuArK  -  Quake Army Knife
 """
 
 #$Header$
-
-
 
 Info = {
    "plug-in":       "Arg Replacer",
@@ -12,8 +10,8 @@ Info = {
    "date":          "6 Sep 99",
    "author":        "Decker",
    "author e-mail": "decker@post1.tele.dk",
-   "quark":         "Version 5.10" }
-
+   "quark":         "Version 5.10"
+}
 
 from quarkpy.maputils import *
 import quarkpy.mapduplicator
@@ -23,51 +21,49 @@ class ArgReplacer(quarkpy.mapduplicator.DuplicatorManager):
     Icon = (quarkpy.mapduplicator.ico_mapdups, 2)
 
     def filterspecs(self, specs):
-	"Removes MACRO and ORIGIN if they exists in the dictspec.keys() array"
-	replacerspecs = []
-	for i in specs:
-	   if (not ((i == "macro") or (i == "origin"))):
-	      replacerspecs = replacerspecs + [i]
-	return replacerspecs
+        "Removes MACRO and ORIGIN if they exists in the dictspec.keys() array"
+        replacerspecs = []
+        for i in specs:
+           if (not ((i == "macro") or (i == "origin"))):
+              replacerspecs = replacerspecs + [i]
+        return replacerspecs
 
     def searchandreplace(self, item, replacerspecs):
-#	print "searchandreplace"
         for r in replacerspecs:
-	   searchstring = '%' + r + '%'
-	   for key in item.dictspec.keys():
-#	      print key, searchstring, item[key], self.dup.dictspec[r]
-	      item[key] = string.replace(item[key], searchstring, self.dup.dictspec[r])
+           searchstring = '%' + r + '%'
+           for key in item.dictspec.keys():
+              item[key] = string.replace(item[key], searchstring, self.dup.dictspec[r])
+              newkey = string.replace(key, searchstring, self.dup.dictspec[r])
+              if (newkey != key):
+                 keyvalue = item[key]
+                 item[key] = None
+                 item[newkey] = keyvalue
 
     def replace(self, items, replacerspecs):
-	for i in items:
-#	   print i.type
-	   if ((i.type == ':g') or (i.type == ':d')):
-	      self.replace(i.subitems, replacerspecs)
-	   elif (i.type == ':e' or i.type == ':b'):
-	      self.searchandreplace(i, replacerspecs)
+        for i in items:
+           if ((i.type == ':g') or (i.type == ':d')):
+              self.replace(i.subitems, replacerspecs)
+           elif (i.type == ':e' or i.type == ':b'):
+              self.searchandreplace(i, replacerspecs)
 
     def buildimages(self, singleimage=None):
-	items = []
-	if ((singleimage is None) or (singleimage == 0)):
-	   if (self.dup.subitems is not None):
-	      # why wont a "items = self.dup.subitems.copy()" work?
-	      for i in self.dup.subitems:
-#		 print "aItem", i
-	         items.append(i.copy())
-#	      print items
-	      replacerspecs = self.filterspecs(self.dup.dictspec.keys())
-#	      print replacerspecs
-	      self.replace(items, replacerspecs)
-	return items
+        items = []
+        if ((singleimage is None) or (singleimage == 0)):
+           if (self.dup.subitems is not None):
+              # why wont a "items = self.dup.subitems.copy()" work?
+              for i in self.dup.subitems:
+                 items.append(i.copy())
+              replacerspecs = self.filterspecs(self.dup.dictspec.keys())
+              self.replace(items, replacerspecs)
+        return items
 
 quarkpy.mapduplicator.DupCodes.update({
-  "arg replacer":	     ArgReplacer,
+  "arg replacer":            ArgReplacer,
 })
 
 # ----------- REVISION HISTORY ------------
 #
-#
 # $Log$
-#
-#
+# Revision 1.2  2000/06/03 10:25:30  alexander
+# added cvs headers
 #
