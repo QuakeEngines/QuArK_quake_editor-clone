fe3a196c9a570e6d7fd45022c5a013bf93d1ecdb
tiglari <tiglari@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2001-07-09 09:53:56 +0000

    update version #

--- source/trunk/prog/QkObjects.pas
+++ source/trunk/prog/QkObjects.pas
@@ -23,6 +23,9 @@ http://www.planetquake.com/quark - Contact information in AUTHORS.TXT
 $Header$
  ----------- REVISION HISTORY ------------
 $Log$
+Revision 1.55  2001/06/18 02:37:08  tiglari
+update version
+
 Revision 1.54  2001/06/05 18:41:26  decker_dk
 Prefixed interface global-variables with 'g_', so its clearer that one should not try to find the variable in the class' local/member scope, but in global-scope maybe somewhere in another file.
 
@@ -190,7 +193,7 @@ uses Windows, SysUtils, Messages, Classes, Clipbrd,
 {$DEFINE ShareSpecMem}
 
 const
-  QuArKVersion            = 'QuArK 6.3snap061801';
+  QuArKVersion            = 'QuArK 6.3snap070901';
 
   iiUnknownFile           = 0;
   iiExplorerGroup         = 1;
