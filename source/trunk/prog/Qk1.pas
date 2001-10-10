8dff9afdf839758710143e25b53b2020030f8992
tiglari <tiglari@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2001-10-10 11:47:49 +0000

    Undo previous change, finalization section is what should be used by freeing   resources allocated in initialization sections of modules

--- source/trunk/prog/Qk1.pas
+++ source/trunk/prog/Qk1.pas
@@ -23,6 +23,9 @@ http://www.planetquake.com/quark - Contact information in AUTHORS.TXT
 $Header$
  ----------- REVISION HISTORY ------------
 $Log$
+Revision 1.24  2001/10/10 11:44:29  tiglari
+attempt to eliminate live pointer by freeing QFileList
+
 Revision 1.23  2001/10/10 10:28:56  tiglari
 attempt to eliminate live pointer error by freeing QObjectClassList on program shutdown
 
@@ -1433,8 +1436,6 @@ begin  { the link to FormDestroy is made in FormCreate }
  end;
  ClearGameBuffers(False);
  ClearPool(True);
- QObjectClassList.Free;
- QFileList.Free;
 end;
 
 procedure TForm1.Saveentryasfile1Click(Sender: TObject);
