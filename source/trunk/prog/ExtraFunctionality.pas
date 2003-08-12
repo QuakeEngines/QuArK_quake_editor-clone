83499e18e1e3c575e81be80abdbea6ed7daa058e
silverpaladin <silverpaladin@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2003-08-12 15:14:46 +0000

    Added Pre-Delphi6  multi platform support routines.

--- source/trunk/prog/ExtraFunctionality.pas
+++ source/trunk/prog/ExtraFunctionality.pas
@@ -1,8 +1,48 @@
+(**************************************************************************
+QuArK -- Quake Army Knife -- 3D game editor
+Copyright (C) Armin Rigo   
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+http://www.planetquake.com/quark - Contact information in AUTHORS.TXT
+**************************************************************************)
+
+{
+$Header$
+----------- REVISION HISTORY ------------
+$Log$
+}
+
 unit ExtraFunctionality;
 
 interface
 uses SysUtils;
 function ConvertPath(const s: string):string;
+
+{$ifndef VER140} // Pre-dates Delphi 6
+
+// IncludeTrailingPathDelimiter returns the path with a PathDelimiter
+//  ('/' or '\') at the end.  This function is MBCS enabled.
+function IncludeTrailingPathDelimiter(const S: string): string;
+
+const
+  PathDelim  = {$IFDEF MSWINDOWS} '\'; {$ELSE} '/'; {$ENDIF}
+  DriveDelim = {$IFDEF MSWINDOWS} ':'; {$ELSE} '';  {$ENDIF}
+  PathSep    = {$IFDEF MSWINDOWS} ';'; {$ELSE} ':'; {$ENDIF}
+
+{$endif}
 
 implementation
 
@@ -15,5 +55,13 @@ begin
   {$ENDIF}
 end;
 
+{$ifndef VER140} // Pre-dates Delphi 6
+function IncludeTrailingPathDelimiter(const S: string): string;
+begin
+  Result := S;
+  if not IsPathDelimiter(Result, Length(Result)) then
+    Result := Result + PathDelim;
+end;
+{$endif}
+
 end.
- 
