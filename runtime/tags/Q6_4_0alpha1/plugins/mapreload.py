03bf6803782c4c26820dddc1bbe7167cc7e6d21e
nobody <nobody@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2003-10-05 08:50:34 +0000

    This commit was manufactured by cvs2svn to create tag 'Q6_4_0alpha1'.

---
+++ runtime/tags/Q6_4_0alpha1/plugins/mapreload.py
@@ -0,0 +1,158 @@
+
+#$Header$
+
+import quarkx
+import quarkpy.mapmenus
+import quarkpy.mapcommands
+import quarkpy.qmacro
+import quarkpy.mapoptions
+
+from quarkpy.maputils import *
+
+
+prevreload = ""
+
+class remodule:
+  "a place to stick stuff"
+
+class ReloadDlg (quarkpy.qmacro .dialogbox):
+    #
+    # dialog layout
+    #
+
+    endcolor = AQUA
+    size = (200,120)
+    dfsep = 0.35
+    flags = FWF_KEEPFOCUS
+    
+    dlgdef = """
+        {
+        Style = "9"
+        Caption = "Reload Dialog"
+
+        module: =
+        {
+        Txt = "reload:"
+        Typ = "EP"
+        DefExt = "py"
+        BasePath = "%splugins"
+        DirSep = "."
+        CutPath = "C:\?\\"
+        Hint = "Type in the name of the module (.py file),"$0D
+               "preceded with its folder name,"$0D
+               "(ex. plugins.mapreload) the .py is optional,"$0D
+               "or just use the file browser ... to the right."$0D
+        }
+
+        sep: = { Typ="S" Txt=" " }
+
+        close:py = {Txt="" }
+        cancel:py = {Txt="" }
+
+    }
+    """%quarkx.exepath  # suggestion by tiglari(the quotes stay)
+
+    #
+    # __init__ initialize the object
+    #
+
+    def __init__(self, form, editor, action):
+
+    #
+    # General initialization of some local values
+    #
+
+        self.editor = editor
+        src = quarkx.newobj(":")
+        self.src = src
+        self.action = action
+        self.form = form
+        self.src["module"] = quarkx.setupsubset(SS_MAP, "Options")["ReloadModule"]
+
+
+    #
+    # Create the dialog form and the buttons
+    #
+
+        quarkpy.qmacro.dialogbox.__init__(self, form, src,
+        close = quarkpy.qtoolbar.button(
+            self.close,
+            "Reload the named module",
+            ico_editor, 2,
+            "Reload"),
+        cancel = quarkpy.qtoolbar.button(
+            self.cancel,
+            "Cancel & close window",
+            ico_editor, 0,
+            "Cancel"))
+
+#    def datachange(self, df):
+#        self.close()   # "OK" is automatic when the user changed the data.
+
+    def onclose(self, dlg):
+        if self.src is None:
+#            quarkx.msgbox("Empty string does not name a module, nothing done", MT_ERROR, MB_OK)
+            qmacro.dialogbox.onclose(self, dlg)
+            return
+        quarkx.globalaccept()
+        self.action(self)
+        qmacro.dialogbox.onclose(self, dlg)
+
+    def cancel(self, dlg):
+        self.src = None 
+        qmacro.dialogbox.close(self, dlg)
+
+
+
+
+def ReloadClick(m):
+  def action(self):
+    if self.src["module"] is None:
+      quarkx.msgbox("Empty string does not name a module, nothing done", MT_ERROR, MB_OK)
+      return
+    module = self.src["module"]
+    quarkx.setupsubset(SS_MAP, "Options")["ReloadModule"] = module
+
+    command = string.replace("reload(%s)"%module, ".py", "")
+    eval(command)
+    
+  editor=mapeditor()
+  if editor is None: return
+  ReloadDlg(quarkx.clickform,editor,action)
+
+quarkpy.mapoptions.items.append(quarkpy.mapoptions.toggleitem("Developer Mode","Developer", hint = "|Developer Mode:\n\nIn this mode, some extra items appear on the menu, to help with debugging, etc.|intro.mapeditor.menu.html#optionsmenu"))
+
+menreload = qmenu.item("Reload",ReloadClick,"Reload module")
+
+if quarkx.setupsubset(SS_MAP, "Options")["Developer"]:
+  quarkpy.mapcommands.items.append(menreload)
+
+
+# ----------- REVISION HISTORY ------------
+#
+#
+# $Log$
+# Revision 1.7  2003/05/13 20:33:29  cdunde
+# To add file browser and correct closing errors
+#
+# Revision 1.6  2003/05/11 16:03:20  cdunde
+# To correct cancel console error
+#
+# Revision 1.5  2003/03/28 02:54:40  cdunde
+# To update info and add infobase links.
+#
+# Revision 1.4  2001/06/17 21:10:57  tiglari
+# fix button captions
+#
+# Revision 1.3  2001/03/20 08:02:16  tiglari
+# customizable hot key support
+#
+# Revision 1.2.4.1  2001/03/11 22:10:42  tiglari
+# customizable hotkeys
+#
+# Revision 1.2  2000/06/03 10:25:30  alexander
+# added cvs headers
+#
+#
+#
+#
---
+++ runtime/tags/Q6_4_0alpha1/quarkpy/mapportals.py
@@ -0,0 +1,156 @@
+"""   QuArK  -  Quake Army Knife
+
+Map editor portal viewer
+"""
+#
+# Copyright (C) 1996-2003 The QuArK community
+# THIS FILE IS PROTECTED BY THE GNU GENERAL PUBLIC LICENCE
+# FOUND IN FILE "COPYING.TXT"
+#
+
+#$Header$
+
+import quarkx
+import qmacro
+import qtoolbar
+from maputils import *
+import mapeditor
+import mapoptions
+
+
+class PortalsDlg(qmacro.dialogbox):
+
+    #
+    # dialog layout
+    #
+
+    dfsep = 0.2
+    dlgflags = 0
+
+    dlgdef = """
+      {
+        Style = "15"
+        Caption = "Portals"
+        sep: = {Typ="S" Bold="0" Txt="Here are the portals from the"}
+        sep: = {Typ="S" Bold="0" Txt="last build, as blue lines."}
+        close:py = {Txt="" }
+      }
+    """
+
+    #
+    # __init__ initialize the object
+    #
+
+    def __init__(self, form, editor):
+        self.editor = editor
+        src = quarkx.newobj(":")
+        qmacro.dialogbox.__init__(self, form, src,
+           close = qtoolbar.button(
+              self.close,
+              "click here to remove portals from your map",
+              ico_editor, 0,
+              "Ok, hide portals"))
+
+    def windowrect(self):
+        x1,y1,x2,y2 = quarkx.screenrect()
+        return (x2-180, 20, x2-15, 132)
+
+    def onclose(self, dlg):
+        try:
+            del self.editor.Portals
+            self.editor.invalidateviews()
+        except:
+            pass
+        qmacro.dialogbox.onclose(self, dlg)
+
+
+def readPortal(pos0, line):
+    result = []
+    while pos0!=-1:
+        pos1 = string.find(line,')',pos0+1)
+        vecstr = line[pos0+1:pos1]
+        vec = quarkx.vect(vecstr)
+        result.append(quarkx.vect(vecstr))
+        pos0 = string.find(line,'(',pos1+1)
+    return result
+
+#
+# This code is very messy due to the chaotic variation in portal
+#  file formats. The Q3 portal files seem rather richer than the
+#  others, see portals.cpp in the bobtoolz source (GtkRadiant)
+#
+def LoadPortalFile(editor, filename):
+    
+    f = open(filename, "r")
+    data = f.readlines()
+    f.close()
+    portals = []
+    index = 0
+    #
+    # skip over introductory lines without actual portals
+    #
+    while index<len(data):
+        pos = string.find(data[index],'(')
+        if pos!=-1:
+            break
+        index = index+1
+    else:
+        quarkx.msgbox('No portals found',MT_INFORMATION,MB_OK)
+        return
+    #
+    # Now read in portal lines beginning with at least three numbers
+    #  these are real, two-sided portals; the ones beginning with 2
+    #  numbers are one-sided; we ignore them, and quit when they appear.
+    #
+    while index<len(data):
+        pos = string.find(data[index],'(')
+        line = data[index]
+        numbers = string.split(line[:pos])
+        if len(numbers)<3:
+            break
+        portals.append(readPortal(pos,line))
+        index=index+1
+    #
+    # And now show the dialog to make them go awayS
+    #
+
+    PortalsDlg(editor.form, editor)
+    editor.Portals = portals
+    editor.invalidateviews()
+
+
+def DrawLines(editor, view, oldFinishDrawing = mapeditor.MapEditor.finishdrawing):
+    try:
+        Portals = editor.Portals
+        cv = view.canvas()
+        cv.pencolor = MapColor("Duplicator")
+        cv.penwidth = mapoptions.getLineThickness()
+        for portal in Portals:
+            pt0 = view.proj(portal[-1])
+            for i in range(len(portal)):
+                pt1 = view.proj(portal[i])
+                cv.line(pt0, pt1)
+                pt0 = pt1
+    except AttributeError:
+        pass
+    oldFinishDrawing(editor, view)
+
+
+mapeditor.MapEditor.finishdrawing = DrawLines
+
+
+#$Log$
+#Revision 1.4  2003/03/19 22:24:10  tiglari
+#fix bug in parsing
+#
+#Revision 1.3  2003/03/19 11:27:47  tiglari
+#remove debug statement
+#
+#Revision 1.2  2003/03/19 11:26:15  tiglari
+#expand info in dialog box
+#
+#Revision 1.1  2003/03/19 11:07:52  tiglari
+#first version, only tested for Q1
+#
+#
+
