127de407a5a400f2b62d6db94192f9186f09367e
alexander <alexander@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2005-01-18 22:54:49 +0000

    initial checkin

---
+++ source/trunk/dllsource/hllib/HLLib/HLLib.h
@@ -0,0 +1,19 @@
+/*
+ * HLLib
+ * Copyright (C) 2004 Ryan Gregg
+
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later
+ * version.
+ */
+
+#ifndef HLLIB_H
+#define HLLIB_H
+
+#include "stdafx.h"
+
+HLLIB_API DWORD GetHLLibVersion();
+
+#endif
