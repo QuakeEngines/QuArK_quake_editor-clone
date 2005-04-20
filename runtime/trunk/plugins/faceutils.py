4f0d7b188b9e9d6a709f962ce0cb3760303bae38
rowdy <rowdy@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2005-04-20 12:31:47 +0000

    added a couple of functions to check for vertices shared by adjacent faces to help with the terrain plugin

--- runtime/trunk/plugins/faceutils.py
+++ runtime/trunk/plugins/faceutils.py
@@ -49,7 +49,7 @@ def abutting_vtx(l1, l2):
   if pozzies[0]==0 and pozzies[1]>1:
     intx.reverse()
   return intx
-    
+
 def intersection_vect(l1, l2):
   "for points/vectors only"
   "note that the points come out in the same order they have in l1"
@@ -71,19 +71,92 @@ def coplanar(f1, f2, opp=1):
     "if opp==0, face normals must point in same direction"
     o1 = f1.dist*f1.normal
     o2 = f2.dist*f2.normal
-#    debug('coplanar')
+    #debug('coplanar')
     if not f1.normal*(o2-o1):
         if not f1.normal-f2.normal:
             return 1
         if opp and not f1.normal+f2.normal:
             return 1
     return 0
-    
+
+def nearly_equals(value1, value2):
+    """Return 1 if the two passed values are almost the same.  This works around
+       rountind errors preventing vertex_in_vertices from working absolutely
+       precisely."""
+    if abs(value1 - value2) < 0.0001:
+        return 1
+    else:
+        return 0
+
+def vertex_in_vertices(v1, vertex_list):
+    """Return 1 if the first vertex is in the list of vertices."""
+    result = 0
+    for vertex in vertex_list:
+        if nearly_equals(v1.x, vertex.x) and \
+           nearly_equals(v1.y, vertex.y) and \
+           nearly_equals(v1.z, vertex.z):
+            result = 1
+    return result
+
+def shared_vertices(selected_faces, all_faces):
+    """searches thru the list of selected_faces and finds each vertex
+       that is shared between a selected face and another face that is
+       not selected, and returns a list of all faces thus detected"""
+
+    result = []
+
+    for selected_face in selected_faces:
+        # TODO: iterate thru all objects that use this face instead of processing
+        #       just the first one
+        selected_polys = selected_face.faceof
+        selected_poly = selected_polys[0]
+        if selected_poly == selected_face:
+            contine # this selected face is not used
+
+        selected_vertices = selected_face.verticesof(selected_poly)
+
+        if not(selected_face in result):
+            result.append(selected_face)
+
+            for a_face in all_faces:
+                if (a_face in selected_faces) or (a_face in result):
+                    continue
+
+                # selected_face is a face that has already been selected
+                # a_face is a face that has not been selected
+                # if these two share at least one vertex, add the unselected
+                #   face to the result list
+
+                found = 0
+
+                # a_face.faceof should return a list of objects that have a_face as one of
+                # their faces.  This is usually just a single poly.  If nothing has a_face as
+                # one of it's faces, then the list contains a_face itself
+                # TODO: iterate thru all objects that use this face instead of processing
+                #       just the first one
+                a_polys = a_face.faceof
+                a_poly = a_polys[0]
+                if a_poly == a_face:
+                    continue # this face is not used
+
+                a_face_vertices = a_face.verticesof(a_poly)
+
+                for selected_vertex in selected_vertices:
+                    if vertex_in_vertices(selected_vertex, a_face_vertices):
+                        found = 1
+                if found:
+                    result.append(a_face)
+
+    return result
+
 
 #$Log$
+#Revision 1.3  2001/08/11 04:14:51  tiglari
+#remove debug
+#
 #Revision 1.2  2001/04/15 06:05:52  tiglari
 #add coplanar function
 #
 #Revision 1.1  2001/04/01 04:43:48  tiglari
 #initial commit
-#       
+#
