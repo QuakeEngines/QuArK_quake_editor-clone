9bb443d79383e348e47ff93bfd1e0af2f55c4960
nobody <nobody@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2005-09-19 00:23:46 +0000

    This commit was manufactured by cvs2svn to create branch 'peter- b_tagging_api'.

---
+++ runtime/branches/peter-b_tagging_api/plugins/mapfulllayout.py
@@ -0,0 +1,53 @@
+"""   QuArK  -  Quake Army Knife
+
+Example Plug-in which define a new screen layout.
+"""
+#
+# Copyright (C) 1996-99 Armin Rigo
+# THIS FILE IS PROTECTED BY THE GNU GENERAL PUBLIC LICENCE
+# FOUND IN FILE "COPYING.TXT"
+#
+
+#$Header$
+
+
+Info = {
+   "plug-in":       "Full-screen 3D Layout",
+   "desc":          "The full-screen 3D wireframe Screen Layout.",
+   "date":          "31 oct 98",
+   "author":        "Armin Rigo",
+   "author e-mail": "arigo@planetquake.com",
+   "quark":         "Version 5.1" }
+
+
+from quarkpy.mapmgr import *
+
+
+#
+# See comments in mapclassiclayout.py.
+#
+
+class Full3DLayout(MapLayout):
+    "The full-screen 3D layout."
+
+    shortname = "Full 3D"
+
+    def buildscreen(self, form):
+        self.bs_leftpanel(form)
+        self.View3D = form.mainpanel.newmapview()
+        self.views[:] = [self.View3D]
+        self.baseviews = self.views[:]
+        self.View3D.info = {"type": "3D"}
+
+
+
+LayoutsList.append(Full3DLayout)
+
+
+# ----------- REVISION HISTORY ------------
+#
+#
+# $Log$
+#
+#
+#
