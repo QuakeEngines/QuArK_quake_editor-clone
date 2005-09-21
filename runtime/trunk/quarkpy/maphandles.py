d6c215906c10c5c86228ad39014b5e762ad92e11
cdunde <cdunde@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2005-09-21 21:33:36 +0000

    To comment out print statement

--- runtime/trunk/quarkpy/maphandles.py
+++ runtime/trunk/quarkpy/maphandles.py
@@ -450,7 +450,7 @@ class SpecialHandle(FaceHandle):
             delta = qhandles.aligntogrid(delta, 0)
         self.draghint = vtohint(delta)
         if delta or (flags&MB_REDIMAGE):
-          print "moved",delta
+           # print "moved",delta
            # new = self.face.copy()
            # if self.face.faceof[0].type == ":p":
            #     delta = self.face.normal * (self.face.normal*delta)  # projection of 'delta' on the 'normal' line
@@ -1918,6 +1918,10 @@ class UserCenterHandle(CenterHandle):
 # ----------- REVISION HISTORY ------------
 #
 #$Log$
+#Revision 1.43  2005/08/18 02:20:50  cdunde
+#Needed to reverse, still under testing of displacement display and handles
+#Does not appear to effect other liner or movement  handle usage.
+#
 #Revision 1.42  2005/08/18 01:17:15  cdunde
 #To comment out 1 print statement left open.
 #
