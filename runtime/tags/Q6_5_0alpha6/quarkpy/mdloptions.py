b7b5cf4505e91eafb89f7fbaddc76201b70eed72
nobody <nobody@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2006-03-07 08:10:45 +0000

    This commit was manufactured by cvs2svn to create tag 'Q6_5_0alpha6'.

---
+++ runtime/tags/Q6_5_0alpha6/quarkpy/mdloptions.py
@@ -0,0 +1,117 @@
+"""   QuArK  -  Quake Army Knife
+
+Implementation of QuArK Model editor's "Options" menu
+"""
+#
+# Copyright (C) 1996-99 Armin Rigo
+# THIS FILE IS PROTECTED BY THE GNU GENERAL PUBLIC LICENCE
+# FOUND IN FILE "COPYING.TXT"
+#
+
+#$Header$
+
+
+
+import quarkx
+from qdictionnary import Strings
+from mdlutils import *
+import qmenu
+
+
+
+def ToggleOption(item):
+    "Toggle an option in the setup."
+    tag = item.tog
+    setup = apply(quarkx.setupsubset, item.sset)
+    newvalue = not setup[tag]
+    setup[tag] = "1"[:newvalue]
+    if item.sendupdate[newvalue]:
+        quarkx.reloadsetup()
+
+
+def Config1Click(item):
+    "Configuration Dialog Box."
+    quarkx.openconfigdlg()
+
+def Plugins1Click(item):
+    "Lists the loaded plug-ins."
+    import plugins
+    group = quarkx.newobj("Loaded Plug-ins:config")
+    for p in plugins.LoadedPlugins:
+        txt = p.__name__.split(".")[-1]
+        ci = quarkx.newobj("%s.toolbar" % txt)
+        try:
+            info = p.Info
+        except:
+            info = {}
+        for spec, arg in info.items():
+            ci[spec] = arg
+        ci["File"] = p.__name__
+        ci["Form"] = "PluginInfo"
+        try:
+            ci.shortname = info["plug-in"]
+        except:
+            pass
+        group.appenditem(ci)
+    quarkx.openconfigdlg("List of Plug-ins", group)
+    
+
+def Options1Click(menu):
+    for item in menu.items:
+        try:
+            setup = apply(quarkx.setupsubset, item.sset)
+            item.state = not (not setup[item.tog]) and qmenu.checked
+        except:
+            pass
+
+def toggleitem(txt, toggle, sendupdate=(1,1), sset=(SS_MODEL,"Options"), hint=None):
+    item = qmenu.item(txt, ToggleOption, hint)
+    item.tog = toggle
+    item.sset = sset
+    item.sendupdate = sendupdate
+    return item
+
+
+#
+# Global variables to update from plug-ins.
+#
+
+items = [
+    toggleitem("Enlarge Vertices &Ticks", "Ticks", (0,0)),
+    toggleitem("&Paste objects at screen center", "Recenter", (0,0)),
+    ]
+shortcuts = { }
+
+
+def OptionsMenu():
+    "The Options menu, with its shortcuts."
+
+    PlugIns = qmenu.item("List of Plug-ins...", Plugins1Click)
+    Config1 = qmenu.item("Confi&guration...", Config1Click)
+    Options1 = qmenu.popup("&Options", items+[qmenu.sep, PlugIns, Config1], Options1Click)
+    return Options1, shortcuts
+
+# ----------- REVISION HISTORY ------------
+#
+#
+#$Log$
+#Revision 1.8  2006/03/06 19:21:23  cdunde
+#To add hint in Infobase linking to toggle
+#Model Editor Optional menu items.
+#
+#Revision 1.7  2005/10/15 00:47:57  cdunde
+#To reinstate headers and history
+#
+#Revision 1.4  2005/08/31 05:36:32  cdunde
+#To add hint argument for toggleitem function.
+#
+#Revision 1.3  2003/12/17 13:58:59  peter-b
+#- Rewrote defines for setting Python version
+#- Removed back-compatibility with Python 1.5
+#- Removed reliance on external string library from Python scripts
+#
+#Revision 1.2  2000/06/02 16:00:22  alexander
+#added cvs headers
+#
+#
+#
---
+++ runtime/tags/Q6_5_0alpha6/quarkpy/qopengl.py
@@ -0,0 +1,168 @@
+"""   QuArK  -  Quake Army Knife
+
+OpenGL manager.
+"""
+#
+# Copyright (C) 1996-2000 Armin Rigo
+# THIS FILE IS PROTECTED BY THE GNU GENERAL PUBLIC LICENCE
+# FOUND IN FILE "COPYING.TXT"
+#
+#$Header$
+
+#
+# NOTE: this module is NEVER actually loaded before an OpenGL
+# view must be opened. To check if an OpenGL view is currently
+# opened DO NOT import qopengl; instead, check the value of
+# qbaselayout.BaseLayout.CurrentOpenGLOwner.
+#
+
+import quarkx
+from qeditor import *
+from qdictionnary import Strings
+from qbasemgr import BaseLayout
+BaseLayout.CurrentOpenGLOwner = None
+
+
+#
+# Only one real OpenGL window is opened at a time;
+# all other map views actually render inside this window (invisibly)
+# and then copy the data to their own surface.
+#
+wnd = None
+glview = None
+offscreen = 0
+
+
+def open(editor, minx=0, miny=0, bkgnd=0, force=0):
+    # open the OpenGL window. If bkgnd=1, open it in the background.
+    # If bkgnd=2, force it in the background.
+
+    global wnd, glview, offscreen
+    #quarkx.settimer(deadtest, None, 0)  # cancel this timer if pending
+    setup = quarkx.setupsubset(SS_GENERAL, "OpenGL")
+
+    if wnd is not None:
+        if force or wnd.owner is not editor.form:
+            wnd.onclose = None
+            close()
+            onclose1(wnd)
+
+    if wnd is None:
+        if setup["Warning2"]:
+            if quarkx.msgbox(Strings[-104], MT_WARNING, MB_YES|MB_NO) != MR_YES:
+                raise quarkx.abort
+        floating = editor.form.newfloating(FWF_NOESCCLOSE, "OpenGL 3D view")
+        view = floating.mainpanel.newmapview()
+        view.info = {"type": "3D", "viewname": "opengl3Dview"}
+        view.viewmode = "opengl"
+        setprojmode(view)
+        floating.onclose = onclose1   # so that onclose1 is called when the window is closed
+        wnd = floating
+        glview = view
+        clearviewdeps()
+        if bkgnd:
+            sw = quarkx.screenrect()[2]   # screen rightmost coordinate
+            wnd.windowrect = (sw+128, 0, sw+448, 200)
+            wnd.rect = (minx or 320, miny or 200)
+        else:
+            r = setup["WndRect"]
+            if safecheckrect(r):
+                wnd.windowrect = r
+                r = r[2:]
+            else:
+                r = (320,200)
+            wnd.rect = (max(r[0],minx), max(r[1],miny))
+        offscreen = bkgnd
+        floating.show()
+        if bkgnd:
+            editor.form.macro("FOCU")    # gives the focus to the map editor window
+
+    else:
+        if not offscreen and bkgnd==2:
+            r = wnd.windowrect
+            r = r[:2] + wnd.rect
+            setup["WndRect"] = r
+            sw = quarkx.screenrect()[2]   # screen rightmost coordinate
+            wnd.windowrect = (sw+128, 0, sw+448, 200)
+            r = r[2:]
+            offscreen = 1
+        elif offscreen and not bkgnd:
+            r = setup["WndRect"]
+            if not safecheckrect(r):
+                r = (0,0,512,384)
+            wnd.windowrect = r
+            r = r[2:]
+            offscreen = 0
+        else:
+            r = wnd.rect
+        r = (max(r[0],minx), max(r[1],miny))
+        if r != wnd.rect:
+            wnd.rect = r
+            glview.waitforopengl()
+
+
+def grayimage(view, *args):
+    cv = view.canvas()
+    cv.brushcolor = 0x604040
+    w,h = view.clientarea
+    cv.rectangle(-1,-1,w,h)
+
+def clearviewdeps():
+    # sets or resets the OpenGL view's parameters
+    v = glview
+    v.ondrop = v.onmouse = lambda *args: None
+    v.ondraw = grayimage
+    v.cursor = CR_ARROW
+
+def safecheckrect(r):
+    sw = quarkx.screenrect()[2]   # screen rightmost coordinate
+    return type(r) is type(()) and r[0]<sw
+
+def close():
+    # close the OpenGL window.
+    if wnd is not None:
+        wnd.close()
+
+
+#def deadtest(*reserved):
+#    # check if the OpenGL window is still in use
+#    if BaseLayout.CurrentOpenGLOwner is None:
+#        close()
+
+
+def onclose1(floating):
+    # called by the Delphi code when the window is closed
+    global wnd, glview
+    if BaseLayout.CurrentOpenGLOwner is not None:
+        BaseLayout.CurrentOpenGLOwner.releaseOpenGL()
+    wnd = glview = None
+    setup = quarkx.setupsubset(SS_GENERAL, "OpenGL")
+    if not offscreen:
+        r = floating.windowrect
+        r = r[:2] + floating.rect
+        setup["WndRect"] = r
+    setup["Warning2"] = ""
+
+
+def setupchanged(level):
+    if level>=5 and wnd is not None:   # change in the configuration dialog box
+        setprojmode(glview)
+
+SetupRoutines.append(setupchanged)
+
+# ----------- REVISION HISTORY ------------
+#
+#
+#$Log$
+#Revision 1.9  2005/10/17 21:27:35  cdunde
+#To add new key word "viewname" to all 3D views for easier
+#detection and control of those views and Infobase documentation.
+#
+#Revision 1.8  2005/10/15 00:47:57  cdunde
+#To reinstate headers and history
+#
+#Revision 1.5  2000/06/02 16:00:22  alexander
+#added cvs headers
+#
+#
+#
