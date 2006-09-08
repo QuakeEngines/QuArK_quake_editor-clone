e89cc129255a15cc40c034783a8774eb1f39df37
nobody <nobody@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2006-09-08 23:45:25 +0000

    This commit was manufactured by cvs2svn to create branch 'DanielPharos'.

---
+++ runtime/branches/DanielPharos/dlls/readme.txt
@@ -0,0 +1,5 @@
+Notice! 
+
+The D3DXAS.DLL is only needed when QuArK is compiled with the {$DEFINE QUARK_DIRECT3D}. If not, then the .DLL file can be deleted from the distribution-package.
+
+The QuArkGCF.dll, QuArkSteamFS.dll, QuArkVTF.dll are only needed for QuArKs Source Engine support and loaded when QuArK must acces the game data.
---
+++ runtime/branches/DanielPharos/plugins/mdllocking.py
@@ -0,0 +1,82 @@
+"""   QuArK  -  Quake Army Knife
+
+Plug-in which allows user to lock axis movement
+of vertices
+"""
+#
+# Copyright (C) 1996-99 Armin Rigo
+# THIS FILE IS PROTECTED BY THE GNU GENERAL PUBLIC LICENCE
+# FOUND IN FILE "COPYING.TXT"
+#
+
+#
+# $Header$
+
+Info = {
+   "plug-in":       "Axis Locking",
+   "desc":          "Axis Locking",
+   "date":          "20 Aug 2000",
+   "author":        "Andy Vincent",
+   "author e-mail": "andyvinc@hotmail.com",
+   "quark":         "Version 6" }
+
+
+import quarkpy.qhandles   
+from quarkpy.mdlmgr import *
+
+def lockxclick(m):
+  editor = mapeditor()
+  quarkpy.qtoolbar.toggle(m)
+  if editor.lock_x == 0:
+    editor.lock_x = 1
+  else:
+    editor.lock_x = 0
+
+def lockyclick(m):
+  editor = mapeditor()
+  quarkpy.qtoolbar.toggle(m)
+  if editor.lock_y == 0:
+    editor.lock_y = 1
+  else:
+    editor.lock_y = 0
+
+def lockzclick(m):
+  editor = mapeditor()
+  quarkpy.qtoolbar.toggle(m)
+  if editor.lock_z == 0:
+    editor.lock_z = 1
+  else:
+    editor.lock_z = 0
+
+#Lock_X = qmenu.item("Lock &X", lockxclick, "lock x axis movement")
+#Lock_Y = qmenu.item("Lock &Y", lockyclick, "lock y axis movement")
+#Lock_Z = qmenu.item("Lock &Z", lockzclick, "lock z axis movement")
+
+class AxisLockBar(ToolBar):
+    "Axis Lock Toolbar"
+
+    Caption = "Axis Lock"
+
+    def buildbuttons(self, layout):
+        LockXBtn = qtoolbar.button(lockxclick, "Lock X Axis", ico_mdled, 0)
+        LockYBtn = qtoolbar.button(lockyclick, "Lock Y Axis", ico_mdled, 1)
+        LockZBtn = qtoolbar.button(lockzclick, "Lock Z Axis", ico_mdled, 2)
+        layout.buttons.update({"lockx": LockXBtn, "locky": LockYBtn,"lockz": LockZBtn})
+        return [LockXBtn, LockYBtn, LockZBtn]
+
+#quarkpy.mdlcommands.items.append(quarkpy.qmenu.sep)
+#quarkpy.mdlcommands.items.append(Lock_X)
+#quarkpy.mdlcommands.items.append(Lock_Y)
+#quarkpy.mdlcommands.items.append(Lock_Z)
+#quarkpy.mdlcommands.shortcuts["Shift+X"] = Lock_X
+#quarkpy.mdlcommands.shortcuts["Shift+Y"] = Lock_Y
+#quarkpy.mdlcommands.shortcuts["Shift+Z"] = Lock_Z
+
+quarkpy.mdltools.toolbars["tb_AxisLock"] = AxisLockBar
+
+# ----------- REVISION HISTORY ------------
+# $Log$
+# Revision 1.3  2000/10/11 19:09:36  aiv
+# added cvs headers
+#
+#
