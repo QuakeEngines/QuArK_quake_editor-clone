67e14bae0c51d28720f9c66a90098d8ff8c901ae
cdunde <cdunde@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2006-10-27 07:15:25 +0000

    Fixed error if Addons Delete item is opened and closed with no items to delete.

--- runtime/trunk/quarkpy/qmacro.py
+++ runtime/trunk/quarkpy/qmacro.py
@@ -215,28 +215,29 @@ class dialogbox:
         name = self.name or self.__class__.__name__
         closedialogbox(name)
         f = quarkx.newobj("Dlg:form")
-        f.loadtext(self.dlgdef)
-        self.f = f
-        for pybtn in f.findallsubitems("", ':py'):
-            pybtn["sendto"] = name
-        self.buttons = buttons
-        dlg = form.newfloating(self.dlgflags, f["Caption"])
-        dialogboxes[name] = dlg
-        dlg.windowrect = self.windowrect()
-        if self.begincolor is not None: dlg.begincolor = self.begincolor
-        if self.endcolor is not None: dlg.endcolor = self.endcolor
-        dlg.onclose = self.onclose
-        dlg.info = self
-        self.dlg = dlg
-        self.src = src
-        df = dlg.mainpanel.newdataform()
-        self.df = df
-        df.header = 0
-        df.sep = self.dfsep
-        df.setdata(src, f)
-        df.onchange = self.datachange
-        df.flags = 8   # DF_AUTOFOCUS
-        dlg.show()
+        if self.dlgdef is not None:
+            f.loadtext(self.dlgdef)
+            self.f = f
+            for pybtn in f.findallsubitems("", ':py'):
+                pybtn["sendto"] = name
+            self.buttons = buttons
+            dlg = form.newfloating(self.dlgflags, f["Caption"])
+            dialogboxes[name] = dlg
+            dlg.windowrect = self.windowrect()
+            if self.begincolor is not None: dlg.begincolor = self.begincolor
+            if self.endcolor is not None: dlg.endcolor = self.endcolor
+            dlg.onclose = self.onclose
+            dlg.info = self
+            self.dlg = dlg
+            self.src = src
+            df = dlg.mainpanel.newdataform()
+            self.df = df
+            df.header = 0
+            df.sep = self.dfsep
+            df.setdata(src, f)
+            df.onchange = self.datachange
+            df.flags = 8   # DF_AUTOFOCUS
+            dlg.show()
 
     def windowrect(self):
         x1,y1,x2,y2 = quarkx.screenrect()
@@ -313,6 +314,9 @@ def MACRO_ent_convertfrom(text):
 # ----------- REVISION HISTORY ------------
 #
 #$Log$
+#Revision 1.20  2005/10/15 00:47:57  cdunde
+#To reinstate headers and history
+#
 #Revision 1.17  2003/12/17 13:58:59  peter-b
 #- Rewrote defines for setting Python version
 #- Removed back-compatibility with Python 1.5
