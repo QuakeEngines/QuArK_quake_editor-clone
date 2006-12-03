c725616b329fde2059ba1d993c25915330e028f7
nobody <nobody@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2006-12-03 18:29:46 +0000

    This commit was manufactured by cvs2svn to create tag 'Q6_5_0Beta1'.

---
+++ runtime/tags/Q6_5_0Beta1/plugins/mdllocking.py
@@ -0,0 +1,147 @@
+"""   QuArK  -  Quake Army Knife
+
+Plug-in which allows user to lock axis movement
+of vertices
+"""
+#
+# Copyright (C) 1996-99 Armin Rigo
+# THIS FILE IS PROTECTED BY THE GNU GENERAL PUBLIC LICENCE
+# FOUND IN FILE "COPYING.TXT"
+#
+
+#
+# $Header$
+
+Info = {
+   "plug-in":       "Axis Locking",
+   "desc":          "Axis Locking",
+   "date":          "20 Aug 2000",
+   "author":        "Andy Vincent",
+   "author e-mail": "andyvinc@hotmail.com",
+   "quark":         "Version 6" }
+
+
+import quarkpy.qhandles   
+from quarkpy.mdlmgr import *
+
+
+def lockxclick(m):
+    editor = mapeditor()
+    quarkpy.qtoolbar.toggle(m)
+    tb1 = editor.layout.toolbars["tb_AxisLock"]
+    if editor.lock_x == 0:
+        editor.lock_x = 1
+        Lock_X.state = 1
+        tb1.tb.buttons[0].state = quarkpy.qtoolbar.selected
+        quarkx.update(editor.form)
+        quarkx.setupsubset(SS_MODEL, "Options")["setLock_X"] = "1"
+    else:
+        editor.lock_x = 0
+        Lock_X.state = 0
+        tb1.tb.buttons[0].state = quarkpy.qtoolbar.normal
+        quarkx.update(editor.form)
+        quarkx.setupsubset(SS_MODEL, "Options")["setLock_X"] = "0"
+
+
+def lockyclick(m):
+    editor = mapeditor()
+    quarkpy.qtoolbar.toggle(m)
+    tb1 = editor.layout.toolbars["tb_AxisLock"]
+    if editor.lock_y == 0:
+        editor.lock_y = 1
+        Lock_Y.state = 1
+        tb1.tb.buttons[1].state = quarkpy.qtoolbar.selected
+        quarkx.update(editor.form)
+        quarkx.setupsubset(SS_MODEL, "Options")["setLock_Y"] = "1"
+    else:
+        editor.lock_y = 0
+        Lock_Y.state = 0
+        tb1.tb.buttons[1].state = quarkpy.qtoolbar.normal
+        quarkx.update(editor.form)
+        quarkx.setupsubset(SS_MODEL, "Options")["setLock_Y"] = "0"
+
+
+def lockzclick(m):
+    editor = mapeditor()
+    quarkpy.qtoolbar.toggle(m)
+    tb1 = editor.layout.toolbars["tb_AxisLock"]
+    if editor.lock_z == 0:
+        editor.lock_z = 1
+        Lock_Z.state = 1
+        tb1.tb.buttons[2].state = quarkpy.qtoolbar.selected
+        quarkx.update(editor.form)
+        quarkx.setupsubset(SS_MODEL, "Options")["setLock_Z"] = "1"
+    else:
+        editor.lock_z = 0
+        Lock_Z.state = 0
+        tb1.tb.buttons[2].state = quarkpy.qtoolbar.normal
+        quarkx.update(editor.form)
+        quarkx.setupsubset(SS_MODEL, "Options")["setLock_Z"] = "0"
+
+
+Lock_X = qmenu.item("Lock &X", lockxclick, "lock x axis movement")  # Commands menu item
+Lock_Y = qmenu.item("Lock &Y", lockyclick, "lock y axis movement")  # Commands menu item
+Lock_Z = qmenu.item("Lock &Z", lockzclick, "lock z axis movement")  # Commands menu item
+
+
+class AxisLockBar(ToolBar):
+    "Creates the Axis Lock Toolbar at startup."
+
+    Caption = "Axis Lock"
+
+    def buildbuttons(self, layout):
+        LockXBtn = qtoolbar.button(lockxclick, "Lock X Axis", ico_mdled, 0)  # tb_AxisLock[0] button
+        LockYBtn = qtoolbar.button(lockyclick, "Lock Y Axis", ico_mdled, 1)  # tb_AxisLock[1] button
+        LockZBtn = qtoolbar.button(lockzclick, "Lock Z Axis", ico_mdled, 2)  # tb_AxisLock[2] button
+        layout.buttons.update({"lockx": LockXBtn, "locky": LockYBtn,"lockz": LockZBtn})
+
+        if quarkx.setupsubset(SS_MODEL, "Options")["setLock_X"]=="1":
+            LockXBtn.state = quarkpy.qtoolbar.selected
+        else:
+            LockXBtn.state = quarkpy.qtoolbar.normal
+
+        if quarkx.setupsubset(SS_MODEL, "Options")["setLock_Y"]=="1":
+            LockYBtn.state = quarkpy.qtoolbar.selected
+        else:
+            LockYBtn.state = quarkpy.qtoolbar.normal
+
+        if quarkx.setupsubset(SS_MODEL, "Options")["setLock_Z"]=="1":
+            LockZBtn.state = quarkpy.qtoolbar.selected
+        else:
+            LockZBtn.state = quarkpy.qtoolbar.normal
+
+        return [LockXBtn, LockYBtn, LockZBtn]
+
+
+quarkpy.mdlcommands.items.append(quarkpy.qmenu.sep)
+quarkpy.mdlcommands.items.append(Lock_X)
+quarkpy.mdlcommands.items.append(Lock_Y)
+quarkpy.mdlcommands.items.append(Lock_Z)
+quarkpy.mdlcommands.shortcuts["Shift+X"] = Lock_X
+quarkpy.mdlcommands.shortcuts["Shift+Y"] = Lock_Y
+quarkpy.mdlcommands.shortcuts["Shift+Z"] = Lock_Z
+
+quarkpy.mdltools.toolbars["tb_AxisLock"] = AxisLockBar
+
+Lock_X.state = int(quarkx.setupsubset(SS_MODEL, "Options")["setLock_X"])
+Lock_Y.state = int(quarkx.setupsubset(SS_MODEL, "Options")["setLock_Y"])
+Lock_Z.state = int(quarkx.setupsubset(SS_MODEL, "Options")["setLock_Z"])
+
+
+# ----------- REVISION HISTORY ------------
+# $Log$
+# Revision 1.7  2006/11/29 06:58:35  cdunde
+# To merge all runtime files that had changes from DanielPharos branch
+# to HEAD for QuArK 6.5.0 Beta 1.
+#
+# Revision 1.6.2.1  2006/11/08 09:24:20  cdunde
+# To setup and activate Model Editor XYZ Commands menu items
+# and make them interactive with the Lock Toolbar.
+#
+# Revision 1.6  2005/10/15 00:51:56  cdunde
+# To reinstate headers and history
+#
+# Revision 1.3  2000/10/11 19:09:36  aiv
+# added cvs headers
+#
+#
---
+++ runtime/tags/Q6_5_0Beta1/quarkpy/mdleditor.py
@@ -0,0 +1,208 @@
+"""   QuArK  -  Quake Army Knife
+
+Core of the Model editor.
+"""
+#
+# Copyright (C) 1996-99 Armin Rigo
+# THIS FILE IS PROTECTED BY THE GNU GENERAL PUBLIC LICENCE
+# FOUND IN FILE "COPYING.TXT"
+#
+
+#$Header$
+
+
+import mdlhandles
+import qhandles
+import mdlmgr
+from qbaseeditor import BaseEditor
+import mdlbtns
+import mdlentities
+
+import qmenu
+import qtoolbar
+import qmacro
+from qeditor import *
+
+#py2.4 indicates upgrade change for python 2.4
+
+class ModelEditor(BaseEditor):
+    "The Model Editor."
+
+    MODE = SS_MODEL
+    manager = mdlmgr
+    ObjectMgr = mdlentities.CallManager
+    HandlesModule = mdlhandles
+    MouseDragMode = mdlhandles.RectSelDragObject
+
+    picked = [ ]
+ 
+    def OpenRoot(self):
+        Root = self.fileobject['Root']
+        if Root is not None:
+            Root = self.fileobject.findname(Root)
+        self.Root = Root
+        if (quarkx.setupsubset(SS_MODEL, "Options")["setLock_X"] is None) and (quarkx.setupsubset(SS_MODEL, "Options")["setLock_Y"] is None) and  (quarkx.setupsubset(SS_MODEL, "Options")["setLock_Z"] is None):
+            Lock_X = "0"
+            Lock_Y = "0"
+            Lock_Z = "0"
+            quarkx.setupsubset(SS_MODEL, "Options")["setLock_X"] = Lock_X
+            quarkx.setupsubset(SS_MODEL, "Options")["setLock_Y"] = Lock_Y
+            quarkx.setupsubset(SS_MODEL, "Options")["setLock_Z"] = Lock_Z
+        else:
+            Lock_X = quarkx.setupsubset(SS_MODEL, "Options")["setLock_X"]
+            Lock_Y = quarkx.setupsubset(SS_MODEL, "Options")["setLock_Y"]
+            Lock_Z = quarkx.setupsubset(SS_MODEL, "Options")["setLock_Z"]
+        self.lock_x = int(quarkx.setupsubset(SS_MODEL, "Options")["setLock_X"])
+        self.lock_y = int(quarkx.setupsubset(SS_MODEL, "Options")["setLock_Y"])
+        self.lock_z = int(quarkx.setupsubset(SS_MODEL, "Options")["setLock_Z"])
+
+        if MldOption("SolidFrame") == "1":
+            for c in self.ListComponents():
+                c.info = { }
+                fillcolor = MapColor("FillColor", SS_MODEL)
+                c.filltris = [(fillcolor,(WHITE,GRAY))]*len(c.triangles)
+        else:
+            pass
+
+    def CloseRoot(self):
+        picked = [ ]
+                
+    def ListComponents(self):
+        return self.Root.findallsubitems("", ':mc')   # find all components
+
+    def initmenu(self, form):
+        "Builds the menu bar."
+        import mdlmenus
+        form.menubar, form.shortcuts = mdlmenus.BuildMenuBar(self)
+        quarkx.update(form)
+
+    def buildhandles(self):
+        "Build the handles for all model views."
+        for v in self.layout.views:
+            v.handles = mdlhandles.BuildHandles(self, self.layout.explorer, v)
+     #   delay, = quarkx.setupsubset(SS_MODEL, "Display")["HandlesDelay"]
+     # linux issue with single quote
+        try:
+            delay, = quarkx.setupsubset(SS_MODEL, "Display")["HandlesDelay"]
+        except:
+            delay = 0.5 # linux issue with single quote
+
+        if delay <= 0.0:
+            commonhandles(self, 0)
+        else:
+#py2.4            quarkx.settimer(commonhandles, self, delay*1000.0)
+            delayfactor = delay*1000
+            quarkx.settimer(commonhandles, self, int(delayfactor))
+
+    def setupchanged(self, level):
+        BaseEditor.setupchanged(self, level)
+        mdlhandles.vertexdotcolor = MapColor("Vertices", SS_MODEL)
+
+    def setupview(self, v, drawmap=None, flags=MV_AUTOFOCUS, copycol=1):
+        BaseEditor.setupview(self, v, drawmap, flags, copycol)
+        if v.info["type"] == "3D":
+            v.cameraposition = (quarkx.vect(150,-100,25), 2.5, 0.0)
+
+
+    def setlayout(self, form, nlayout):
+        BaseEditor.setlayout(self, form, nlayout)
+        if nlayout is not None:
+            for obj in self.Root.subitems:
+                if obj.type == ':mc':      # Expand the Component objects
+                     nlayout.explorer.expand(obj)
+                     
+
+    def dropmap(self, view, newlist, x, y, src):
+        center = view.space(x, y, view.proj(view.screencenter).z)
+        mdlbtns.dropitemsnow(self, newlist, center=center)
+
+
+    def explorermenu(self, reserved, view=None, origin=None):
+        "The pop-up menu for the Explorer."
+
+        import mdlmenus
+        sellist = self.layout.explorer.sellist
+        if len(sellist)==0:
+            return mdlmenus.BackgroundMenu(self, view, origin)
+        if view is None:
+            extra = []
+        else:
+            extra = [qmenu.sep] + mdlmenus.TexModeMenu(self, view)
+        if len(sellist)==1:
+            return mdlentities.CallManager("menu", sellist[0], self) + extra
+        return mdlmenus.MultiSelMenu(sellist, self) + extra
+
+
+    def explorerdrop(self, ex, list, text):
+        return mdlbtns.dropitemsnow(self, list, text)
+
+    def explorerinsert(self, ex, list):
+        for obj in list:
+            mdlbtns.prepareobjecttodrop(self, obj)
+
+    def editcmdclick(self, m):
+        # dispatch the command to mdlbtns' "edit_xxx" procedure
+        getattr(mdlbtns, "edit_" + m.cmd)(self, m)
+
+    def deleteitems(self, list):
+        mdlbtns.deleteitems(self.Root, list)
+
+
+    def ForceEverythingToGrid(self, m):
+        mdlbtns.ForceToGrid(self, self.gridstep, self.layout.explorer.sellist)
+
+    def moveby(self, text, delta):
+        mdlbtns.moveselection(self, text, delta)
+
+
+
+def commonhandles(self, redraw=1):
+    if self.layout is None: return
+    hlist = mdlhandles.BuildCommonHandles(self, self.layout.explorer)   # handles common to all views
+    for v in self.layout.views:
+        v.handles = hlist + v.handles
+        if redraw:
+            cv = v.canvas()
+            for h in hlist:
+                h.draw(v, cv, None)
+
+# ----------- REVISION HISTORY ------------
+#
+#
+#$Log$
+#Revision 1.11  2006/11/29 07:00:27  cdunde
+#To merge all runtime files that had changes from DanielPharos branch
+#to HEAD for QuArK 6.5.0 Beta 1.
+#
+#Revision 1.10.2.3  2006/11/08 09:24:20  cdunde
+#To setup and activate Model Editor XYZ Commands menu items
+#and make them interactive with the Lock Toolbar.
+#
+#Revision 1.10.2.2  2006/11/04 21:40:30  cdunde
+#To stop Python 2.4 Depreciation message in console.
+#
+#Revision 1.10.2.1  2006/11/03 23:38:10  cdunde
+#Updates to accept Python 2.4.4 by eliminating the
+#Depreciation warning messages in the console.
+#
+#Revision 1.10  2006/03/07 04:51:41  cdunde
+#Setup model frame outlining and options for solid and color selection.
+#
+#Revision 1.9  2006/01/30 08:20:00  cdunde
+#To commit all files involved in project with Philippe C
+#to allow QuArK to work better with Linux using Wine.
+#
+#Revision 1.8  2005/10/15 00:47:57  cdunde
+#To reinstate headers and history
+#
+#Revision 1.5  2001/03/15 21:07:49  aiv
+#fixed bugs found by fpbrowser
+#
+#Revision 1.4  2000/08/21 21:33:04  aiv
+#Misc. Changes / bugfixes
+#
+#Revision 1.2  2000/06/02 16:00:22  alexander
+#added cvs headers
+#
+#
+#
