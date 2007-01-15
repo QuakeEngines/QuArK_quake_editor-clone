2dd6559aba0a5baad1648ce689ae800acb6452b7
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2007-01-15 19:57:22 +0000

    Synced with HEAD cvs

--- source/branches/DanielPharos/prog/model/qkskindrawobject.pas
+++ source/branches/DanielPharos/prog/model/qkskindrawobject.pas
@@ -23,6 +23,9 @@ http://www.planetquake.com/quark - Contact information in AUTHORS.TXT
 $Header$
 ----------- REVISION HISTORY ------------
 $Log$
+Revision 1.8  2005/09/28 10:49:02  peter-b
+Revert removal of Log and Header keywords
+
 Revision 1.6  2001/06/05 18:42:41  decker_dk
 Prefixed interface global-variables with 'g_', so its clearer that one should not try to find the variable in the class' local/member scope, but in global-scope maybe somewhere in another file.
 
@@ -59,7 +62,7 @@ type
 
 implementation
 
-uses Ed3dfx, PyMapView, quarkx, travail, pyobjects, QkModelRoot, qkComponent, setup, QkObjectClassList;
+uses EdSoftware, PyMapView, quarkx, travail, pyobjects, QkModelRoot, qkComponent, setup, QkObjectClassList;
 
 procedure QSkinDrawObject.CouleurDessin;
 var
