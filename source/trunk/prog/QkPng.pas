d7b037e5ae3d629b64f7142613985bb95ce00051
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2007-02-07 18:45:38 +0000

    Updated PNG units to version 1.56. There was a resource leak in the old version.

--- source/trunk/prog/QkPng.pas
+++ source/trunk/prog/QkPng.pas
@@ -23,6 +23,9 @@ http://www.planetquake.com/quark - Contact information in AUTHORS.TXT
 $Header$
  ----------- REVISION HISTORY ------------
 $Log$
+Revision 1.7  2007/02/07 14:31:50  danielpharos
+Fixed a typo
+
 Revision 1.6  2005/09/28 10:48:32  peter-b
 Revert removal of Log and Header keywords
 
@@ -78,17 +81,17 @@ end;
 
 procedure QPng.LoadFile(F: TStream; FSize: Integer);
 var
-  png: TPNGImage;
+  png: TPNGObject;
   BitmapStruct: Windows.TBitmap;
   BmpInfo: TBitmapInfo256;
   BitmapInfo: TBitmapInfo absolute BmpInfo;
   Source: String;
   sss : string;
-  bpp{, BaseMemSize}: Integer;
+  {BaseMemSize: Integer;}
 begin
   case ReadFormat of
     1: begin  { as stand-alone file }
-      png:=TPNGImage.Create;
+      png:=TPNGObject.Create;
       try 
         SetLength(sss, F.Size);
         F.ReadBuffer(sss[1], F.Size);
@@ -96,34 +99,28 @@ begin
         F.Seek(0, 0);
         png.LoadFromStream(F);
         {PasteBitmap(GameBuffer(mjAny), png);}
- FillChar(BmpInfo, SizeOf(BmpInfo), 0);
- with BmpInfo.bmiHeader do
-  begin
-   biSize:=SizeOf(TBitmapInfoHeader);
-   biWidth:=png.Width;
-   biHeight:=png.Height;
-   biPlanes:=1;
-  end;
- //not used (and the only hint when building QuArK): BaseMemSize:=((png.Width+3) and not 3) * png.Height;
- GetObject(png.Handle, SizeOf(BitmapStruct), @BitmapStruct);
- bpp:=BitmapStruct.bmBitsPixel*BitmapStruct.bmPlanes;
- if bpp=8 then
-  begin
-        {Write me}
-  end
- else
-  begin
-   BmpInfo.bmiHeader.biBitCount:=24;
-   SetLength(Source, ((png.Width*3+3) and not 3) * png.Height);
-   GetDIBits(png.Canvas.Handle, png.Handle, 0, png.Height,
-    PChar(Source), BitmapInfo, dib_RGB_Colors);
+        FillChar(BmpInfo, SizeOf(BmpInfo), 0);
+        with BmpInfo.bmiHeader do
+         begin
+          biSize:=SizeOf(TBitmapInfoHeader);
+          biWidth:=png.Width;
+          biHeight:=png.Height;
+          biBitCount:=24;
+          biPlanes:=1;
+         end;
+        //not used (and the only hint when building QuArK): BaseMemSize:=((png.Width+3) and not 3) * png.Height;
+        GetObject(png.Header.ImageHandleValue, SizeOf(BitmapStruct), @BitmapStruct);
+        SetLength(Source, ((png.Width*3+3) and not 3) * png.Height);
+        GetDIBits(png.Canvas.Handle, png.Header.ImageHandleValue, 0, png.Height,
+          PChar(Source), BitmapInfo, dib_RGB_Colors);
 
-   SetSize(Point(png.Width, png.Height));
-   Specifics.Values['Image1']:=Source;
-   Specifics.Values['raw_png_data']:= sss;
-   {SetString(PalStr, PChar(@Lmp), SizeOf(TPaletteLmp));
-   Specifics.Values['Pal']:=PalStr;}
-  end;
+        {DanielPharos: Loading of PNG files that are not 24 bits is giving problems...}
+
+        SetSize(Point(png.Width, png.Height));
+        Specifics.Values['Image1']:=Source;
+        Specifics.Values['raw_png_data']:= sss;
+        {SetString(PalStr, PChar(@Lmp), SizeOf(TPaletteLmp));
+        Specifics.Values['Pal']:=PalStr;}
       finally
         png.Free;
       end;
@@ -136,7 +133,7 @@ end;
 procedure QPng.SaveFile(Info: TInfoEnreg1);
 (*
 var
-  png: TPngImage;
+  png: TPNGObject;
 *)  
 var
    data: String;
