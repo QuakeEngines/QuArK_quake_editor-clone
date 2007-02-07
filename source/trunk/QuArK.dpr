cafd4a16cb5aa9cfa0706d2024fc7bec4882fc65
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2007-02-07 16:51:48 +0000

    Fixed a typo.

--- source/trunk/QuArK.dpr
+++ source/trunk/QuArK.dpr
@@ -23,6 +23,11 @@ http://www.planetquake.com/quark - Contact information in AUTHORS.TXT
 $Header$
  ----------- REVISION HISTORY ------------
 $Log$
+Revision 1.35  2007/01/02 21:01:05  danielpharos
+To put back the original memory manager. The leaks are killing QuArK!
+Added FastMM 4.74 for debugging purposes
+MemCheck 2.74 for debugging purposes
+
 Revision 1.34  2006/12/31 21:58:16  danielpharos
 Upgraded to FastMM 4.74.This replaces the old MemTester file. FastMM should be faster, cleaner and better for debugging.
 
@@ -92,7 +97,7 @@ program QuArK;
 uses
   MemTester in 'Prog\MemTester.pas',
   {FastMM4 in 'Prog\FastMM4.pas',}     {Enable for FastMM}
-  {MemCheck in 'Prog\MemCheck.pas',}     {Enable for MemCheck}
+  {MemCheck in 'Prog\MemCheck.pas',}     {Enable for MemCheck, also see below}
   Forms,
   QkObjects in 'Prog\QkObjects.pas',
   Qk1 in 'Prog\Qk1.pas' {Form1},
@@ -249,7 +254,7 @@ uses
 {$R *.RES}
 
 begin
-  {MemChk}     {Enable for MemCheck}
+  {MemChk;}     {Enable for MemCheck}
   Application.Initialize;
   Application.Title:='Quake Army Knife';
   Application.CreateForm(TForm1, Form1);
