dce5bfd7d579bb29a04157606d1ab0fc00b2c7eb
cdunde <cdunde@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2008-04-15 11:08:53 +0000

    To add main and all sub-folders level processing of texture listing.

--- games/trunk/generic_game_setup_tools/TextureList.py
+++ games/trunk/generic_game_setup_tools/TextureList.py
@@ -1,68 +1,85 @@
 # TextureList.py - Makes a games .qrk list of Textures by folder to use as a QuArK addon game file.
 #            - cdunde Feb. 25, 2008
 #            - Put this file and the main "textures" folder (with its sub-folders) in the same directory.
-#            - Change "dirname", "GameName", "GameFolder" and "TexFileType" below.
+#            - Change "QuArKpath", "gamename", "GameFolder" and "texturesfiletype" below.
 #            - Start the "DOS Command Window", cd (change directory) to the path location.
 #            - Type in "python TextureList.py" and hit Enter.
 #            - The completed file will be created in the same location as this file is.
+#            - This code will process files in the main "textures" folder and all sub-folders on down.
 
 import os, os.path, string
 
-dirname = "c:\\cdunde_FAKK2"  ### Set your work folder (where this file is) Path here.
-GameName = "FAKK2"  ### Set the game name here.
-GameFolder = "fakk"  ### Set the game FOLDER name here (where the games .pak or "textures" folder is).
-TexFileType = ".ftx"  ### Set the textures file type here.
+QuArKpath = "c:\\cdunde_textures_test"  ### Set your work folder (where this file is) Path here.
+gamename = "Quake 2"  ### Set the game name here.
+addonfolder = gamename.replace(" ","_")
+WorkDirectory = (QuArKpath + '\\' + addonfolder)  ### Sets work folder (where .qrk file will be) Path here.
+GameFolder = "baseq2"  ### Set the game FOLDER name here (where the games .pak or "textures" folder is).
+texturesfiletype = ".jpg;*.tga; *.wal"  ### Set the textures file type here.
 
-o = open(dirname + "\\" + GameName + "textures.qrk", "w")  ### Change the path here.
+texturesfolder = (QuArKpath + '\\textures')
+gamefileslocation = (QuArKpath)
 
-foldernames = os.listdir(dirname + "\\textures")
-count = 0
+TexFileTypeList = texturesfiletype
+TexFileTypeList = TexFileTypeList.replace(" ","")
+TexFileTypeList = TexFileTypeList.replace("*","")
+TexFileTypeList = TexFileTypeList.split(";")
+
+if not os.path.exists(QuArKpath + '\\' + addonfolder):
+    os.mkdir(QuArKpath + '\\' + addonfolder)
+
+### Write the textures list:
+def listfiles(basefolder, foldername, filenames):
+    count = 0
+    for name in filenames:
+        for filetype in TexFileTypeList:
+            if filetype.lower() in name.lower():
+                if count == 0:
+                    foldername = foldername.replace(texturesfolder + "\\", "")
+                    foldername = foldername.replace("\\", "/")
+                    o.write("      " + foldername + ".txlist =\n")
+                    o.write("      {\n")
+                    count = 1
+                shortname = string.rsplit(name, ".", 1)  ### This removes the file type suffix.
+                o.write("        " + foldername + "/" + shortname[0] + ".wl")
+                line = len("        " + foldername + "/" + shortname[0] + ".wl")
+                while line < 51:
+                    o.write(" ")
+                    line = line + 1
+                o.write("= { t_" + GameFolder + "=! }\n")
+    if count == 1:
+        o.write("      }\n")
+
+filenames = []
+
+o = open(WorkDirectory + "\\" + gamename + "Textures.qrk", "w")
 
 ### Writes the new .qrk file header.
 o.write("QQRKSRC1\n")
-o.write("// " + GameName + " textures file for Quark\n")
+o.write("// " + gamename + " Textures file for Quark\n")
 o.write("\n")
-o.write("//$Header$\n")
+o.write("//$" + "Header: Exp $" + "\n")
 o.write("// ----------- REVISION HISTORY ------------\n")
-o.write("//$Log$\n")
+o.write("//$" + "Log: " + gamename + "Textures.qrk,v $" + "\n")
 o.write("//\n")
 
 ### Writes the setup part for the Texture Browser folders and needed path "include".
 o.write("\n")
 o.write("{\n")
 o.write("  QuArKProtected = \"1\"\n")
-o.write("  Description = \"" + GameName + " Textures\"\n")
+o.write("  Description = \"" + gamename + " Textures\"\n")
 o.write("\n")
 o.write("  Textures.qtx =\n")
 o.write("  {\n")
 o.write("    Toolbox = \"Texture Browser...\"\n")
-o.write("    Root = \"" + GameName + " Textures.qtxfolder\"\n")
+o.write("    Root = \"" + gamename + " Textures.qtxfolder\"\n")
 o.write("\n")
 o.write("    t_" + GameFolder + ":incl =      { a = \"" + GameFolder + "\" }\n")
 o.write("\n")
-o.write("    " + GameName + " Textures.qtxfolder =\n")
+o.write("    " + gamename + " Textures.qtxfolder =\n")
 o.write("    {\n")
 
 ### Writes all the texture sub-folders textures list here.
-for folder in foldernames:
-    currentdir = dirname + "\\textures\\" + folder
-    filenames = os.listdir(currentdir)
-    for file in filenames:
-        if file.endswith(TexFileType):
-            if count == 0:
-                o.write("      " + folder + ".txlist =\n")
-                o.write("      {\n")
-                count = 1
-            shortname = string.replace(file, TexFileType, "")  ### This removes the file type suffix.
-            o.write("        " + folder + "/" + shortname + ".wl")
-            line = len("        " + folder + "/" + shortname + ".wl")
-            while line < 51:
-                o.write(" ")
-                line = line + 1
-            o.write("= { t_" + GameFolder + "=! }\n")
-    if count == 1:
-        o.write("      }\n")
-        count = 0
+os.path.walk(texturesfolder, listfiles, filenames)
 
 ### Finishes writing the closing part of the new .qrk file here and closes it.
 o.write("    }\n")
