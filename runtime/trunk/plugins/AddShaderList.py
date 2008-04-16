5ab53eeee8c5abfed3542a386ee95719b5b4106b
cdunde <cdunde@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2008-04-16 22:25:22 +0000

    Updated processing for Quake 4 type shaders.

--- runtime/trunk/plugins/AddShaderList.py
+++ runtime/trunk/plugins/AddShaderList.py
@@ -5,6 +5,7 @@ AddShaderList.py - Makes the GameShaders.qrk list of Shaders by folder to use as
             - This file is used by the QuArK ConvertionTool on the QuArK Explorer main Files menu.
             - The completed file will be created in the game's folder in QuArK's main folder.
             - The game's "shaders", "scripts" or "materials" folder (with its sub-folders) must be extracted.
+            - This code will process files in the main "shaders" folder and all sub-folders on down.
 """
 
 #
@@ -13,6 +14,9 @@ AddShaderList.py - Makes the GameShaders.qrk list of Shaders by folder to use as
 
 #
 #$Log$
+#Revision 1.6  2008/04/11 18:33:20  cdunde
+#Changed code to try to adapt to proper version writing.
+#
 #Revision 1.5  2008/04/06 06:47:42  cdunde
 #Added file back without version control to stop overwriting of internal code.
 #
@@ -31,19 +35,18 @@ AddShaderList.py - Makes the GameShaders.qrk list of Shaders by folder to use as
 #
 #
 
+###Globals
+skip = []
+text = "" # This is a temporary place in memory to copy an existing file's data to and
+          # add to it, or write new entity data to for making an new file later.
+
 import os, os.path
 from ConvertToolGet_tokens import *
 
 def AddShaders(QuArKpath, gamename, gamefileslocation, shadersfolder, shadersfiletype):
-    def shortName(s):
-        result = s
-        q = s.find(".")
-        if q != -1:
-            result = s[:q]
-        return result
-
+    global skip, text
     WorkDirectory = (QuArKpath + '\\' + gamename)  ### Sets work folder (where .qrk file will be) Path here.
-    ### Set the shader's file folder name here.
+    ### Sets the shader's file folder name here.
     filesfoldername = shadersfolder.split('\\')
     filesfoldername = filesfoldername[len(filesfoldername)-1]
     whatkind = filesfoldername.capitalize()  ### Sets what they are called here.
@@ -51,47 +54,150 @@ def AddShaders(QuArKpath, gamename, gamefileslocation, shadersfolder, shadersfil
     GameFolder = gamefileslocation.split('\\')
     GameFolder = GameFolder[len(GameFolder)-1]
 
-    names = os.listdir(shadersfolder)
-    names.sort()
+    ### Write the includes list:
+    def listincludes(basefolder, foldername, filenames):
 
-    nested = 0
-    material_name = "undefined"
-    kount = 0
+        def shortName(s):
+            result = s
+            q = s.find(".")
+            if q != -1:
+                result = s[:q]
+            return result
 
-    ### Does a name removal for files that do not have any "texture/" item in them, nothing we can use.
-    tempnameslist = names
-    for name in names:
-        tokens = getTokens(gamefileslocation + "\\" + filesfoldername + "\\" + name)
-        iToken = iter(tokens)
-        tokenType, tokenValue = iToken.next()
-        hastextures = 0
-        while tokenType != T_EOF:
-            if (tokenType == T_SPECIAL) and (tokenValue == "{"):
-                # this is the start of a new material
-                nested = 1
-                kount += 1
-                while nested > 0:
-                    tokenType, tokenValue = iToken.next()
-                    if tokenType == T_EOF:
-                        break
-                    if (tokenType == T_SPECIAL) and (tokenValue == "{"):
-                        nested += 1
-                    elif (tokenType == T_SPECIAL) and (tokenValue == "}"):
-                        nested -= 1
+        nested = 0
+        material_name = "undefined"
+        kount = 0
 
-                # for now, ignore anything that does not start with 'textures/'
-                if material_name.startswith('textures/'):
-                    hastextures = 1
-                    break
-
-            if tokenType == T_IDENTIFIER:
-                material_name = tokenValue
-
+        ### Does a name removal for files that do not have any "texture/" item in them, nothing we can use.
+        tempnameslist = filenames
+        for name in filenames:
+            if not name.endswith(shadersfiletype):
+                continue
+            tokens = getTokens(foldername + "\\" + name)
+            iToken = iter(tokens)
             tokenType, tokenValue = iToken.next()
+            hastextures = 0
+            while tokenType != T_EOF:
+                if (tokenType == T_SPECIAL) and (tokenValue == "{"):
+                    # this is the start of a new material
+                    nested = 1
+                    kount += 1
+                    while nested > 0:
+                        tokenType, tokenValue = iToken.next()
+                        if tokenType == T_EOF:
+                            break
+                        if (tokenType == T_SPECIAL) and (tokenValue == "{"):
+                            nested += 1
+                        elif (tokenType == T_SPECIAL) and (tokenValue == "}"):
+                            nested -= 1
 
-        if hastextures == 0:
-            tempnameslist.remove(name)
-    names = tempnameslist
+                    # for now, ignore anything that does not start with 'textures/'
+                    if material_name.startswith('textures/'):
+                        hastextures = 1
+                        break
+
+                if tokenType == T_IDENTIFIER:
+                    material_name = tokenValue
+
+                tokenType, tokenValue = iToken.next()
+
+            if hastextures == 0:
+                tempnameslist.remove(name)
+        filenames = tempnameslist
+
+        ### Calculates the longest shader filename so we can pad the incl lines to make it look nice.
+        longestName = 0
+        for name in filenames:
+            if not name.endswith(shadersfiletype):
+                continue
+            if len(name) > longestName:
+                longestName = len(name)
+        longestName += 14
+
+        ### Writes the 'include' part for each folder that has a shader file with a 'textures/' name in it.
+        for name in filenames:
+            if not name.endswith(shadersfiletype):
+                continue
+            if shortName(name) in skip:
+                continue
+            s = 't_' + GameFolder + filesfoldername + '_%s:incl' % shortName(name)
+            while len(s) < longestName:
+                s += ' '
+            ### Subfolder's name gets added here.
+            folder = foldername.replace(shadersfolder, "")
+            if folder != "":
+                folder = folder.replace("\\", "")
+                folder = folder + '/'
+            o.write(('    %s = { a="' + GameFolder + '" b="' + folder + name + '" }\n') % (s))
+
+    def listfiles(basefolder, foldername, filenames):
+        global skip, text
+
+        def shortName(s):
+            result = s
+            q = s.find(".")
+            if q != -1:
+                result = s[:q]
+            return result
+
+        nested = 0
+        material_name = "undefined"
+        kount = 0
+
+        ### Writes the shader files list now for each folder.
+        for name in filenames:
+            hasitem = 0
+            if not name.endswith(shadersfiletype):
+                continue
+            tokens = getTokens(foldername + "\\" + name)
+            iToken = iter(tokens)
+            tokenType, tokenValue = iToken.next()
+            while tokenType != T_EOF:
+                if (tokenType == T_SPECIAL) and (tokenValue == "{"):
+                    # this is the start of a new material
+                    nested = 1
+                    kount += 1
+                    while nested > 0:
+                        tokenType, tokenValue = iToken.next()
+                        if tokenType == T_EOF:
+                            break
+                        if (tokenType == T_SPECIAL) and (tokenValue == "{"):
+                            nested += 1
+                        elif (tokenType == T_SPECIAL) and (tokenValue == "}"):
+                            nested -= 1
+
+                    # For now, ignore anything that does not start with 'textures/'
+                    # and use its include statement created above.
+                    if material_name.startswith('textures/'):
+                        if hasitem == 0:
+                            text = text + ('      %s.txlist =\n') % shortName(name)
+                            text = text + '      {\n'
+                            hasitem = 1
+                        text = text + ('        %s.wl    = { t_' + GameFolder + filesfoldername + '_%s=! }\n') % (material_name[9:], shortName(name))
+                # Quake 4 (a Doom3 based game) uses 'guide' as a keyword.
+                # If found does not use its include statment created above,
+                # it uses what it finds in the input file for the shader texture instead.
+                if tokenValue.startswith('guide'):
+                    tokenType, tokenValue = iToken.next()
+                    if tokenValue.startswith('textures/'):
+                        if hasitem == 0:
+                            text = text + ('      %s.txlist =\n') % shortName(name)
+                            text = text + '      {\n'
+                            hasitem = 1
+                        tokenValue = tokenValue.replace('textures/', "")
+                        text = text + ('        %s.wl    = { a="' + GameFolder + '" n="%s_d" }\n') % (tokenValue, tokenValue)
+
+                if tokenType == T_IDENTIFIER:
+                    material_name = tokenValue
+
+                tokenType, tokenValue = iToken.next()
+
+            if hasitem != 0:
+                text = text + '      }\n'
+            else:
+                skip = skip + [shortName(name)]
+
+    filenames = []
 
     o = open(WorkDirectory + "\\" + gamename + whatkind + ".qrk", "w")
 
@@ -116,60 +222,19 @@ def AddShaders(QuArKpath, gamename, gamefileslocation, shadersfolder, shadersfil
     o.write("    Root = \"" + gamename + " " + whatkind + ".qtxfolder\"\n")
     o.write("\n")
 
-    ### Calculates the longest shader filename so we can pad the incl lines to make it look nice.
-    longestName = 0
-    for name in names:
-        if len(name) > longestName:
-            longestName = len(name)
-    longestName += 14
-
-    ### Writes the 'include' part for each folder that has a shader file with a 'textures/' name in it.
-    for name in names:
-        s = 't_' + GameFolder + filesfoldername + '_%s:incl' % shortName(name)
-        while len(s) < longestName:
-            s += ' '
-        ### Subfolder's name gets added here.
-        o.write(('    %s = { a="' + GameFolder + '" b="%s' + shadersfiletype + '" }\n') % (s, shortName(name)))
+    ### Writes all the texture sub-folders textures list here.
+    os.path.walk(shadersfolder, listfiles, filenames)
+    os.path.walk(shadersfolder, listincludes, filenames)
     o.write('\n')
     o.write('    ' + gamename + ' ' + whatkind + '.qtxfolder =\n')
     o.write('    {\n')
+    o.write(text)
+    skip = []
+    text = ""
 
-    ### Writes the shader files list now for each folder using its own include created above.
-    for name in names:
-        o.write(('      %s.txlist =\n') % shortName(name))
-        o.write('      {\n')
-
-        tokens = getTokens(gamefileslocation + "\\" + filesfoldername + "\\" + name)
-        iToken = iter(tokens)
-        tokenType, tokenValue = iToken.next()
-        while tokenType != T_EOF:
-            if (tokenType == T_SPECIAL) and (tokenValue == "{"):
-                # this is the start of a new material
-                nested = 1
-                kount += 1
-                while nested > 0:
-                    tokenType, tokenValue = iToken.next()
-                    if tokenType == T_EOF:
-                        break
-                    if (tokenType == T_SPECIAL) and (tokenValue == "{"):
-                        nested += 1
-                    elif (tokenType == T_SPECIAL) and (tokenValue == "}"):
-                        nested -= 1
-
-                # for now, ignore anything that does not start with 'textures/'
-                if material_name.startswith('textures/'):
-                    o.write(('        %s.wl    = { t_' + GameFolder + filesfoldername + '_%s=! }\n') % (material_name[9:], shortName(name)))
-
-            if tokenType == T_IDENTIFIER:
-                material_name = tokenValue
-
-            tokenType, tokenValue = iToken.next()
-
-        o.write('      }\n')
-
+    ### Finishes writing the closing part of the new .qrk file here and closes it.
     o.write('    }\n')
     o.write('  }\n')
     o.write('}\n')
-
     o.close()
 
