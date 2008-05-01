114af6aba1d9ce37f6079445972ba5eb19ac4b43
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2008-05-01 12:58:59 +0000

    Added copyright header and other small fixes.

--- source/trunk/prog/BrowseForFolder.pas
+++ source/trunk/prog/BrowseForFolder.pas
@@ -1,7 +1,31 @@
+(**************************************************************************
+QuArK -- Quake Army Knife -- 3D game editor
+Copyright (C) Armin Rigo
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+http://www.planetquake.com/quark - Contact information in AUTHORS.TXT
+**************************************************************************)
+
 {
 $Header$
  ----------- REVISION HISTORY ------------
 $Log$
+Revision 1.1  2008/05/01 12:57:41  danielpharos
+Moved BrowseForFolder to a better place.
+
 Revision 1.9  2008/04/17 15:19:05  cdunde
 Fixed 'Folder Browser' to stop displaying the 'Hint' and only show the 'Txt'.
 
@@ -24,11 +48,13 @@ fix for retail cs.
 Revision 1.2  2000/09/10 14:05:21  alexander
 added cvs headers
 
-
 }
+
 unit BrowseForFolder;
 
 interface
+
+{$I DelphiVer.inc}
 
 uses Windows;
 
