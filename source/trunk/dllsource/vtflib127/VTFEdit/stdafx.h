d825564f38c70fcf440be1890c306dd672326649
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2008-05-01 10:09:26 +0000

    Updated VTFLib to version 1.2.7

---
+++ source/trunk/dllsource/vtflib127/VTFEdit/stdafx.h
@@ -0,0 +1,60 @@
+/*
+ * VTFEdit
+ * Copyright (C) 2005-2008 Neil Jedrzejewski & Ryan Gregg
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#pragma once
+
+#if _MSC_VER >= 1400
+#	define _CRT_SECURE_NO_WARNINGS
+#	define _CRT_NONSTDC_NO_DEPRECATE
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <richedit.h>
+
+#include "..\lib\VTFLib.h"
+#pragma comment(lib, "..\\..\\..\\lib\\VTFLib.lib")
+
+#include "..\lib\HLLib.h"
+#pragma comment(lib, "..\\..\\..\\lib\\HLLib.lib")
+
+#include "..\lib\il.h"
+#pragma comment(lib, "..\\..\\..\\lib\\DevIL.lib")
+
+#ifdef CreateDirectory
+#undef CreateDirectory
+#endif
+
+#ifdef GetObject
+#undef GetObject
+#endif
+
+#ifdef GetTempPath
+#undef GetTempPath
+#endif
+
+#ifdef MessageBox
+#undef MessageBox
+#endif
