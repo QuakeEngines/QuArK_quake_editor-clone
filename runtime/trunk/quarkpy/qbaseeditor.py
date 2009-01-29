fc22eb6913c74a4e3ff7fbef912b2f5f8618269c
cdunde <cdunde@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2009-01-29 02:12:41 +0000

    Fix by DanielPharos for skins not changing before Skin-view is opened.

--- runtime/trunk/quarkpy/qbaseeditor.py
+++ runtime/trunk/quarkpy/qbaseeditor.py
@@ -110,13 +110,6 @@ class BaseEditor:
             scale1=1.0
         import mdleditor
         if isinstance(self, mdleditor.ModelEditor):
-            # To try to set the correct skin if the Skin-view has not been opened yet, especially for a bone drag.
-            from mdlhandles import SkinView1
-            if SkinView1 is None:
-                try: # In case there is no skin we need this to be a try statement to avoid an error and lockup.
-                    self.Root.currentcomponent.currentskin = self.Root.currentcomponent.dictitems['Skins:sg'].subitems[0]
-                except:
-                    pass
             # Stops duplicate drawing of handles in all views after a zoom in a 3D view.
             if view.info["type"] != "3D" and (view.info["viewname"] != "editors3Dview" and view.info["viewname"] != "3Dwindow"):
                 try:
@@ -1567,6 +1560,9 @@ NeedViewError = "this key only applies to a 2D map view"
 #
 #
 #$Log$
+#Revision 1.123  2009/01/10 03:54:42  cdunde
+#Minor error fix in case there are no skins.
+#
 #Revision 1.122  2009/01/05 09:43:37  cdunde
 #To try to set the correct skin if the Skin-view has not been opened yet, especially for a bone drag.
 #
