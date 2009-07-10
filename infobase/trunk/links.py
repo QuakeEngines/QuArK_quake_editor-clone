cb4188f2ccfa3ed54b09962e533ce62a713b3100
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2009-07-10 09:29:10 +0000

    Fixed link to communication-page.

--- infobase/trunk/links.py
+++ infobase/trunk/links.py
@@ -9,6 +9,7 @@ linksdict["website"] = mainwebsite
 linksdict["forums"] = mainwebsite + "forums/"
 linksdict["infobase"] = mainwebsite + "infobase/"
 linksdict["news"] = mainwebsite + "news.php3"
+linksdict["communication"] = mainwebsite + "communication.php3"
 linksdict["download"] = mainwebsite + "download.php3"
 linksdict["project"] = "http://sourceforge.net/projects/quark"
 linksdict["cvs"] = "http://quark.cvs.sourceforge.net/viewvc/quark/"
