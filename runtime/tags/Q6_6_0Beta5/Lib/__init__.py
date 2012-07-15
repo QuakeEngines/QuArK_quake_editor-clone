4b80016cbe6b27d5e2880e1de3f027d5475fecf4
nobody <nobody@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2012-07-15 20:40:48 +0000

    This commit was manufactured by cvs2svn to create tag 'Q6_6_0Beta5'.

---
+++ runtime/tags/Q6_6_0Beta5/Lib/__init__.py
@@ -0,0 +1,22 @@
+# Two lines below to stop encoding errors in the console.
+#!/usr/bin/python
+# -*- coding: ascii -*-
+
+"""   QuArK  -  Quake Army Knife
+
+Libs init
+"""
+
+#$Header$
+
+
+#
+#This file is to make sure we can load Python files from this directory when needed.
+#
+
+
+
+# ----------- REVISION HISTORY ------------
+#
+#
+# $Log$
