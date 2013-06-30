e13f88da4f4d1acd108f8ddda4c1eb1395dda2a3
nobody <nobody@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2013-06-30 15:18:33 +0000

    This commit was manufactured by cvs2svn to create tag 'Q6_6_0Beta6'.

---
+++ runtime/tags/Q6_6_0Beta6/plugins/devpyobjects.py
@@ -0,0 +1,39 @@
+
+#$Header$
+
+# -----
+
+#How to use this file:
+#
+# 1) Import it using the console:
+#    > import plugins.devpyobjects
+#
+# 2) Run it:
+#    > plugins.devpyobjects.OutputPyObjects()
+#
+#
+# The output file will be dropped in QuArK's log directory.
+#
+# -----
+#
+# If you want to compare different logs (for instance, in order to find python object leaks),
+# you can specify the filename (without extension) of the output log, so you can easily make a 'before' and 'after' log.
+#
+
+import quarkx
+
+def OutputPyObjects(filename='python_objects'):
+    import gc
+    f = open(quarkx.logpath+filename+'.log', 'w')
+    try:
+        f.write(str(gc.get_objects()))
+    finally:
+        f.close()
+    print 'OutputPyObjects: DONE!'
+
+# ----------- REVISION HISTORY ------------
+#
+# $Log$
+# Revision 1.1  2010/02/21 20:30:08  danielpharos
+# Added a plugin for outputting all current Python objects in memory (see file for usage).
+#
