97d1b5ac1437ddd7e43f2d7b5379d7172a22df74
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2015-01-09 20:22:09 +0000

    Changed default hotkey of Rebuild3D to a more conventional one.

--- runtime/trunk/plugins/mdl3drebuild.py
+++ runtime/trunk/plugins/mdl3drebuild.py
@@ -24,7 +24,7 @@ from quarkpy.qutils import *
 
 
 Rebuild3Ds = quarkpy.mdloptions.toggleitem("&Rebuild 3D views", "Rebuild3D", (1,1),
-      hint="|Rebuild 3D views in Model Editor:\n\nThis rebuilds the 3D views (actually all views) in the Model Editor in case of a lockup. You may have to do this a few times to clear the views up.\n\nThe easiest way is to just push the HotKey 'Tab' until the views unlock and clear up.|intro.modeleditor.menu.html#optionsmenu")
+      hint="|Rebuild 3D views in Model Editor:\n\nThis rebuilds the 3D views (actually all views) in the Model Editor in case of a lockup. You may have to do this a few times to clear the views up.\n\nThe easiest way is to just push the HotKey until the views unlock and clear up.|intro.modeleditor.menu.html#optionsmenu")
 
 quarkpy.mdloptions.items.append(Rebuild3Ds)
 for menitem, keytag in [(Rebuild3Ds, "Rebuild3D")]:
@@ -42,6 +42,9 @@ quarkpy.mdleditor.ModelEditor.finishdrawing = newfinishdrawing
 # ----------- REVISION HISTORY ------------
 #
 #$Log$
+#Revision 1.7  2008/02/22 09:52:21  danielpharos
+#Move all finishdrawing code to the correct editor, and some small cleanups.
+#
 #Revision 1.6  2006/11/30 01:17:48  cdunde
 #To fix for filtering purposes, we do NOT want to use capital letters for cvs.
 #
