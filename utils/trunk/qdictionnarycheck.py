aa3c310fc6947c2c5e3a5954dce19fc801f07404
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2015-12-05 16:03:49 +0000

    Small clean-up of code.

--- utils/trunk/qdictionnarycheck.py
+++ utils/trunk/qdictionnarycheck.py
@@ -2,19 +2,19 @@
 
 numbers = []
 i = open("qdictionnary.py")
-while 1:
-    s = i.readline()
-    if s == "":
-        break
-    s = s.strip()
-    c = s.split(":")
-    try:
-        number = int(c[0])
-        if number in numbers:
-            print "duplicate on line: %s" % s
-        else:
-            numbers.append(number)
-    except:
-        pass
-i.close()
-
+try:
+    for s in i.readlines():
+        if s == "":
+            break
+        s = s.strip()
+        c = s.split(":")
+        try:
+            number = int(c[0])
+            if number in numbers:
+                print "duplicate on line: %s" % (s, )
+            else:
+                numbers.append(number)
+        except:
+            pass
+finally:
+    i.close()
