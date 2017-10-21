b3ea006deff05c6ca391f0227be50901c8614104
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2017-10-21 12:16:07 +0000

    Update compiledate for new nightly release.

--- source/trunk/prog/QkConsts.pas
+++ source/trunk/prog/QkConsts.pas
@@ -23,6 +23,9 @@ http://quark.sourceforge.net/ - Contact information in AUTHORS.TXT
 $Header$
  ----------- REVISION HISTORY ------------
 $Log$
+Revision 1.11  2017/05/05 16:53:21  danielpharos
+Updated copyright year.
+
 Revision 1.10  2017/02/25 14:41:25  danielpharos
 Updated compile-date for new nightly build.
 
@@ -95,7 +98,7 @@ const
 {$ENDIF}
 {$ENDIF}
 {$ENDIF}
-  QuArKCompileDate        = 42791;   //This is the compiled date
+  QuArKCompileDate        = 43029;   //This is the compiled date
   { Amount of days that have passed after 30 Dec 1899 (Delphi 2+).
     You can use EncodeDate(Year, Month, Day) to compute it, but this value
     really needs to be a constant, so put the resulting value in here.
