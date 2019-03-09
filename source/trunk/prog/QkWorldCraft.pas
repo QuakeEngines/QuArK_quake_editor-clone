dc7e411948bc880c78b386856e427aaf15813ec8
danielpharos <danielpharos@5419a3ea-6bc3-475c-b79b-167d7c3fbd5f>
2019-03-09 13:38:17 +0000

    Improvements to .rmf file loading: many more things are now loading properly, including groups, entities with brushes, and the origin of entities.

--- source/trunk/prog/QkWorldCraft.pas
+++ source/trunk/prog/QkWorldCraft.pas
@@ -61,7 +61,7 @@ type
   end;
 
   RMFTexture_33 = packed record //@ UNTESTED
-    texture: array[0..MAX_PATH-1] of Byte; 
+    texture: array[0..MAX_PATH-1] of Byte;
     UAxis: array[0..3] of Single;
     VAxis: array[0..3] of Single;
     rotate: Single;
@@ -75,10 +75,8 @@ type
     nLightmapScale: Integer;
   end;
 
-//@: See http://www.inxbus.net/hldoc/d6/d51/loadsave__rmf_8cpp-source.html
-const
-//VERSION:  $CD, $CC, $0C, $40,
-  RMFVersion: Single = 3.7;
+const //@
+  RMFMaxVersion: Single = 3.7;
   RMFLastCompatVersion: Single = 0.3;
   RmfID: array[0..2] of Byte = (Byte('R'), Byte('M'), Byte('F'));
   old_group_bytes: Integer = 134;
@@ -99,13 +97,12 @@ end;
 
 procedure QRmfMapFile.LoadFile(F: TStream; FSize: Integer);
 
-  function ReadRMFString(var F: TStream; FSize: Integer) : String;
+  function ReadRMFString : String;
   var
     StringLength: Byte;
   begin
-    FillChar(StringLength, SizeOf(Byte), 0);
-    F.ReadBuffer(StringLength, SizeOf(Byte)); 
-    if StringLength > 128 then
+    F.ReadBuffer(StringLength, SizeOf(Byte));
+    if StringLength >= 128 then
       Log(LOG_INFO, LoadStr1(5787));
     SetLength(Result, StringLength);
     if StringLength>0 then
@@ -122,8 +119,6 @@ procedure QRmfMapFile.LoadFile(F: TStream; FSize: Integer);
 
 var
  Racine: TTreeMapBrush;
- Entite, EntitePoly: TTreeMapSpec;
- Entities, MapStructure: TTreeMapGroup;
  ModeJeu: Char;
 
  BrushNum, FaceNum: Integer;
@@ -141,24 +136,41 @@ var
  DummyDWORD: DWORD;
  i: Integer;
 
- procedure LoadMapClass; forward;
- procedure LoadEditGameClass; forward;
+ procedure LoadMapClass(var obj : TTreeMap); forward;
+ procedure LoadEditGameClass(var obj : TTreeMap); forward;
 
- procedure LoadMapEntity;
+ procedure LoadMapEntity(parent: TTreeMap);
+ var
+   Entite: TTreeMapSpec;
+   Origin: array[0..2] of Single;
+   OldPos: Integer;
+   IsBrush: Boolean;
  begin
-   //FIXME
-   //if (SymbolType<>sCurlyBracketLeft) and (HullNum=-1) then
-   //  Entite:=TTreeMapEntity.Create(Classname, Entities)
-   //else
-     Entite:=TTreeMapBrush.Create('CMapEntity', Entities); //FIXME
-   Entities.SubElements.Add(Entite);
-   EntitePoly:=Entite;
+   //Ugly hack; we need to know if there's children for this entity
+   OldPos:=F.Position;
+   if RMFVersion < 1.0 then
+   begin
+     F.ReadBuffer(DummyInteger, SizeOf(Integer));
+     F.Seek(DummyInteger + 3 * SizeOf(Byte), soFromCurrent);
+   end
+   else
+     F.Seek(SizeOf(DWORD) + 3 * SizeOf(Byte), soFromCurrent);
+   F.ReadBuffer(DummyInteger, SizeOf(Integer));
+   F.Seek(OldPos, soFromBeginning);
+   IsBrush:=(DummyInteger<>0);
+   if IsBrush then
+     Entite:=TTreeMapBrush.Create('CMapEntity', parent) //Temp name
+   else
+     Entite:=TTreeMapEntity.Create('CMapEntity', parent); //Temp name
+   parent.SubElements.Add(Entite);
 
-   LoadMapClass;
-   LoadEditGameClass;
+   LoadMapClass(TTreeMap(Entite));
+   LoadEditGameClass(TTreeMap(Entite));
 
    F.ReadBuffer(DummyWORD, SizeOf(WORD)); //flags
-   F.ReadBuffer(DummyVector[0], 3*SizeOf(Single)); //origin
+   F.ReadBuffer(Origin[0], 3*SizeOf(Single));
+   if not IsBrush then
+     TTreeMapEntity(Entite).Origin:=MakeVect(Origin[0], Origin[1], Origin[2]);
 
    if RMFVersion < 0.5 then
      DummyVector[2]:=-DummyVector[2];
@@ -166,17 +178,22 @@ var
    F.ReadBuffer(DummyInteger, SizeOf(Integer)); //complex (unused)
  end;
 
- procedure LoadMapGroup;
+ procedure LoadMapGroup(parent: TTreeMap);
+ var
+   Entite: TTreeMapSpec;
  begin
-   LoadMapClass;
+   Entite:=TTreeMapGroup.Create('func_group', parent); //Slightly naughty hardcoded name
+   parent.SubElements.Add(Entite);
+
+   LoadMapClass(TTreeMap(Entite));
  end;
 
- procedure LoadMapHelper;
+ procedure LoadMapHelper(parent: TTreeMap);
  begin
    //@
  end;
 
- procedure LoadMapDisp;
+ procedure LoadMapDisp();
  begin
    //@
  end;
@@ -287,7 +304,7 @@ var
 
      //FIXME: OldTex33.smooth
      //FIXME: OldTex33.material
-     //FIXME: OldTex33.q2surface
+     //FIXME: OldTex33.q2surface @@@Deze kunnen al!
      //FIXME: OldTex33.q2contents
      //FIXME: OldTex33.nLightmapScale
      //FIXME if texture.nLightmapScale == 0 then Use Default Value from GameConfig
@@ -326,21 +343,21 @@ var
        F.ReadBuffer(DummyBOOL, SizeOf(DummyBOOL)); //bHasMapDisp;
 
      if DummyBool then
-       LoadMapDisp;
+       LoadMapDisp();
    end;
    //@
  end;
 
- procedure LoadMapSolid;
+ procedure LoadMapSolid(parent: TTreeMap);
  var
    i: Integer;
    P: TPolyhedron;
  begin
-   LoadMapClass;
-   FaceNum:=0;
+   P:=TPolyhedron.Create(LoadStr1(138), parent);
+   parent.SubElements.Add(P);
 
-   P:=TPolyhedron.Create(LoadStr1(138), EntitePoly);
-   EntitePoly.SubElements.Add(P);
+   LoadMapClass(TTreeMap(P));
+   FaceNum:=0;
 
    F.ReadBuffer(DummyInteger, SizeOf(Integer)); //Number of faces
    for i := 0 to DummyInteger-1 do
@@ -348,10 +365,10 @@ var
      LoadMapFace(P);
      FaceNum:=FaceNum+1;
    end;
-     //@
+   //@
  end;
 
- procedure LoadMapClass;
+ procedure LoadMapClass(var obj : TTreeMap);
  var
    i: Integer;
  begin
@@ -360,7 +377,7 @@ var
    begin
      // kill group information .. unfortunate
      F.ReadBuffer(DummyInteger, SizeOf(Integer));
-     F.Seek(DummyInteger, 1);
+     F.Seek(DummyInteger, soFromCurrent);
    end
    else
    begin
@@ -377,16 +394,16 @@ var
    F.ReadBuffer(DummyInteger, SizeOf(Integer));
    for i := 0 to DummyInteger-1 do
    begin
-     DummyString:=ReadRMFString(F, FSize); //Name of pChild
+     DummyString:=ReadRMFString(); //Name of pChild
      if DummyString = 'CMapEntity' then
-       LoadMapEntity
+       LoadMapEntity(obj)
      else if DummyString = 'CMapGroup' then
-       LoadMapGroup
+       LoadMapGroup(obj)
      else if DummyString = 'CMapHelper' then //used?
-       LoadMapHelper
+       LoadMapHelper(obj)
      else if DummyString = 'CMapSolid' then
      begin
-       LoadMapSolid();
+       LoadMapSolid(obj);
        BrushNum:=BrushNum+1;
      end
      else
@@ -394,26 +411,28 @@ var
    end;
  end;
 
- procedure LoadKeyValue;
+ procedure LoadKeyValue(var obj : TTreeMap);
  begin
-   DummyString:=ReadRMFString(F, FSize); //Key
-   DummyString:=ReadRMFString(F, FSize); //Value
+   DummyString:=ReadRMFString(); //Key
+   DummyString:=ReadRMFString(); //Value
    //@
  end;
 
- procedure LoadEditGameClass;
+ procedure LoadEditGameClass(var obj : TTreeMap);
  var
+   ClassName: String;
    i: Integer;
  begin
-   DummyString:=ReadRMFString(F, FSize);
+   ClassName:=ReadRMFString();
    F.ReadBuffer(DummyInteger, SizeOf(Integer)); //Angle
    F.ReadBuffer(DummyInteger, SizeOf(Integer)); //Spawnflags
-   if DummyString[1] = #0 then
+   if ClassName[1] = #0 then
      Log(LOG_INFO, 'RMF: Invalid EditGameClass name.'); //@
+   obj.Name:=ClassName;
    F.ReadBuffer(DummyInteger, SizeOf(Integer)); //Size
    for i := 0 to DummyInteger-1 do
    begin
-     LoadKeyValue;
+     LoadKeyValue(obj);
      //@
    end;
    //@ OLD angle...?
@@ -426,12 +445,16 @@ var
    end;
  end;
 
- procedure LoadMapPath;
+ procedure LoadMapPath(parent : TTreeMap);
  var
+   Entite: TTreeMapSpec;
    DummyBuffer: array[0..127] of Char;
    DummyInteger2: Integer;
    i, k: Integer;
  begin
+   Entite:=TTreeMapEntity.Create('CMapPath', parent); //Temp name
+   parent.SubElements.Add(Entite);
+
    F.ReadBuffer(DummyBuffer, 128); //128 = SizeOf(DummyBuffer) //Name
    F.ReadBuffer(DummyBuffer, 128); //128 = SizeOf(DummyBuffer) //Class
    F.ReadBuffer(DummyInteger, SizeOf(DummyInteger)); //Direction
@@ -446,7 +469,7 @@ var
        F.ReadBuffer(DummyBuffer, 128); //128 = SizeOf(DummyBuffer) //Name
        F.ReadBuffer(DummyInteger2, SizeOf(DummyInteger2)); //size
        for k := 0 to DummyInteger2-1 do
-         LoadKeyValue;
+         LoadKeyValue(TTreeMap(Entite));
      end;
    end;
  end;
@@ -460,16 +483,11 @@ begin
         ModeJeu:=mjHalfLife;
         g_MapError.Clear;
 
-        Entities:=TTreeMapGroup.Create(LoadStr1(136), Racine);
-        Racine.SubElements.Add(Entities);
-        MapStructure:=TTreeMapGroup.Create(LoadStr1(137), Racine);
-        Racine.SubElements.Add(MapStructure);
-
         //LoadMapWorld:
         if FSize<SizeOf(RMFVersion) then
           Raise EError(5519);
         F.ReadBuffer(RMFVersion, SizeOf(RMFVersion));
-        if (RMFVersion < RMFLastCompatVersion) or (RMFVersion > RMFVersion) then
+        if (RMFVersion < RMFLastCompatVersion) or (RMFVersion > RMFMaxVersion) then
           Raise EErrorFmt(5779, [LoadStr1(5780)]);
         if RMFVersion >= 0.8 then
         begin
@@ -490,24 +508,22 @@ begin
         end;
 
         // make sure it's a CMapWorld
-        DummyString:=ReadRMFString(F, FSize);
+        DummyString:=ReadRMFString();
         if DummyString<>'CMapWorld' then
           Raise EErrorFmt(5779, [FmtLoadStr1(5782, [DummyString, 'CMapWorld'])]);
         Racine.Name:=DummyString;
-        Entite:=Racine;
-        EntitePoly:=MapStructure;
 
         // load children & local data
-        LoadMapClass;
+        LoadMapClass(TTreeMap(Racine));
 
         // load ceditgameclass & CMapClass
-        LoadEditGameClass;
+        LoadEditGameClass(TTreeMap(Racine));
 
         if RMFVersion < 1.0 then
         begin
           //Not supported at all; skip this data
           F.ReadBuffer(DummyInteger, SizeOf(Integer));
-          F.Seek(old_group_bytes * DummyInteger, 1);
+          F.Seek(old_group_bytes * DummyInteger, soFromCurrent);
         end;
 
         // load paths
@@ -515,7 +531,7 @@ begin
         begin
           F.ReadBuffer(DummyInteger, SizeOf(Integer));
           for i := 0 to DummyInteger-1 do
-            LoadMapPath;
+            LoadMapPath(TTreeMap(Racine));
         end;
 
         // read camera
@@ -525,15 +541,14 @@ begin
           F.ReadBuffer(DummyVector[0], 3*SizeOf(Single)); //unused
         end;
 
+        Racine.FixupAllReferences;
+
         SubElements.Add(Racine);
         Specifics.Values['Root']:=Racine.Name+Racine.TypeInfo;
         ObjectGameCode:=ModeJeu;
       finally
         Racine.AddRef(-1);
       end;
-
-      //@     Racine.FixupAllReferences;?
-
      end;
  else
   inherited;
@@ -544,7 +559,7 @@ procedure QRmfMapFile.SaveFile(Info: TInfoEnreg1);
 begin
  with Info do case Format of
   rf_Default: begin  { as stand-alone file }
-      raise EQObjectSavingNotSupported.Create('Saving RMF files is currently not supported.');
+      raise EQObjectSavingNotSupported.Create('Saving RMF files is currently not supported.'); //FIXME: Move to dict!
      end;
  else
   inherited;
